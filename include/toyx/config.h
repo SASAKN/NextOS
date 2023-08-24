cbc91e534ffc7149a831d20831bafbb5ea834582
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-08-24 22:06:50 +0900

    基本情報を追加

---
+++ include/toyx/config.h
@@ -0,0 +1,10 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+/* OS release and version numbers  */
+#define OS_NAME "Toyx" /* OS Name */
+#define OS_RELEASE "1" /* OS Version */
+#define OS_VERSION "1.0.0" /* OS Release */
+
+
+#endif
