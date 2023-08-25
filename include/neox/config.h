1d44c10cdf1ae638930ea34e4e449da7081a91b8
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-08-25 18:31:11 +0900

    これでOK

--- include/neox/config.h
+++ include/neox/config.h
@@ -2,14 +2,15 @@
 #define _CONFIG_H
 
 /* OS release and version numbers  */
-#define OS_NAME "neox" /* OS Name */
-#define OS_RELEASE "1" /* OS Version */
+#define OS_NAME "neox"     /* OS Name */
+#define OS_CODENAME "none" /* OS CodeName */
+#define OS_RELEASE "1"     /* OS Version */
 #define OS_VERSION "1.0.0" /* OS Release */
 
 /* Machine Definitions */
 #include <neox/machine.h>
 
-/* */
+/* Settings */
 
 
 
