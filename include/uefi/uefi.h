5adfd2d5bec8b3fd0b9b12530b6f81a5531dc755
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-08-28 22:47:05 +0900

    UEFIの定義を追加

--- include/uefi/uefi.h
+++ include/uefi/uefi.h
@@ -81,12 +81,39 @@ typedef struct {
 
 typedef struct {
     uint64_t _buf;
-    uint64_t 
+    uint64_t (*output_string) (SimpleTextOutputProtocol*, const wchar_t*);
+    uint64_t (*test_string) (SimpleTextOutputProtocol*, wchar_t*);
+    uint64_t (*query_mode) (SimpleTextOutputProtocol*,
+                            wchar_t*,
+                            uint64_t* columns,
+                            uint64_t* rows);
+    uint64_t (*set_mode) (SimpleTextOutputProtocol*, uint64_t);
+    uint64_t (*set_attribute) (SimpleTextOutputProtocol*, uint64_t Attribute);
+    uint64_t (*clear_screen) (SimpleTextOutputProtocol*);
+    uint64_t _buf4[2];
+    struct SIMPLE_TEXT_OUTPUT_MODE{
+        int MaxMode;
+        int Mode;
+        int Attribute;
+        int CursorColumn;
+        int CursorRow;
+        uint8_t CursorVisible;
+    } * Mode;
 } SimpleTextOutputProtocol;
 
 typedef struct {
-
-} SimpleTextOutputProtocol;
+    uint16_t year;
+    uint8_t mon;
+    uint8_t day;
+    uint8_t hour;
+    uint8_t min;
+    uint8_t sec;
+    uint8_t p1;
+    uint32_t ns;
+    uint16_t tz;
+    uint8_t dl;
+    uint8_t p2;
+} tm;
 
 
 
