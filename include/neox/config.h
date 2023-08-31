4212fd4c22d24752af20b219966effe50088e225
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-08-31 18:44:46 +0900

    デベロッパーリリースであることを追加

--- include/neox/config.h
+++ include/neox/config.h
@@ -2,10 +2,12 @@
 #define _CONFIG_H
 
 /* OS release and version numbers  */
-#define OS_NAME "neox"     /* OS Name */
-#define OS_CODENAME "none" /* OS CodeName */
-#define OS_RELEASE "1"     /* OS Version */
-#define OS_VERSION "1.0.0" /* OS Release */
+#define OS_NAME "neox"     //OS Name
+#define OS_CODENAME "none" //OS CodeName
+#define OS_RELEASE "1"     //OS Version
+#define OS_VERSION "1.0.0" //OS Release
+#define OS_DEVELOPER "SASAKEN" //OS Developer
+#define OS_EDITION "DEV" //OS Editions (DEV, NONE, RELEASE)
 
 
 /* Machine Definitions */
