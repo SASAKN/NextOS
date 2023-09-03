70a86776df3daa5b3d8bf96596569badca0e2e80
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-03 22:35:51 +0900

    色々追加

--- arch/uefi/types.h
+++ arch/uefi/types.h
@@ -18,6 +18,12 @@
 #define EFIAPI
 #endif
 
+/* GUID */
+#define EFI_BOOT_MANAGER_POLICY_PROTOCOL_GUID \
+  { 0xFEDF8E0C, 0xE147, 0x11E3,\
+  { 0x99, 0x03, 0xB8, 0xE8, 0x56, 0x2C, 0xBA, 0xFA } }
+
+
 /* Boolean */
 typedef unsigned char Boolean;
 
@@ -59,6 +65,43 @@ typedef unsigned short CHAR16;
 
 /* VOID */
 typedef void VOID;
+
+/* EFI_INPUT_KEY */
+typedef struct {
+    UINT16 ScanCode;
+    UINT16 UnicodeChar;
+} EFI_INPUT_KEY;
+
+
+/* EFI_KEY_OPTION */
+typedef struct {
+  EFI_BOOT_KEY_DATA         KeyData;
+  UINT32                    BootOptionCrc;
+  UINT16                    BootOption;
+} EFI_KEY_OPTION;
+
+/* EFI_BOOT_KEY_DATA */
+typedef union {
+  struct {
+    UINT32   Revision : 8;
+    UINT32   ShiftPressed : 1;
+    UINT32   ControlPressed : 1;
+    UINT32   AltPressed : 1;
+    UINT32   LogoPressed : 1;
+    UINT32   MenuPressed : 1;
+    UINT32   SysReqPressed : 1;
+    UINT32   Reserved : 16;
+    UINT32   InputKeyCount : 2;
+    } Options;
+  UINT32 PackedValue;
+} EFI_BOOT_KEY_DATA;
+
+/* EFI_MEMORY_TYPE */
+typedef enum {
+    EfiReservedMemoryType,
+    EfiLoaderCode
+} EFI_MEMORY_TYPE;
+
 
 /* EFI_GUID */
 typedef struct {
