4bbaeae4edc8207e02e185d660490e1d969a5bae
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-08 22:12:33 +0900

    これでANSI Cじゅんきょ

--- include/neox/config.h
+++ include/neox/config.h
@@ -2,13 +2,13 @@
 #define _CONFIG_H
 
 /* OS release and version numbers  */
-#define OS_NAME "neox"     //OS Name
-#define OS_CODENAME "none" //OS CodeName
-#define OS_RELEASE "1"     //OS Version
-#define OS_VERSION "1.0.0" //OS Release
-#define OS_DEVELOPER "SASAKEN" //OS Developer
-#define OS_EDITION "DEV" //OS Editions (DEV, NONE, RELEASE)
-#define OS_PLATFORM "ALL" //OS PLATFORMS (UEFI_X64, ALL)
+#define OS_NAME "neox"     /* OS Name */
+#define OS_CODENAME "none" /* OS Codename */
+#define OS_RELEASE "1"     /* OS Version */
+#define OS_VERSION "1.0.0" /* OS Release */
+#define OS_DEVELOPER "SASAKEN" /* OS Developer */
+#define OS_EDITION "DEV" /* OS Editions (DEV, NONE, RELEASE) */
+#define OS_PLATFORM "ALL" /*OS PLATFORMS (UEFI_X64, ALL) */
 
 
 /* Machine Definitions */
--- include/neox/types.h
+++ include/neox/types.h
@@ -1,16 +1,16 @@
 #ifndef _TYPES_H
 #define _TYPES_H
 
-//True Or False
+/* True Or False */
 
 #define TRUE 1
 #define FALSE 0
 
-//Char Types
+/* Char Types */
 typedef unsigned char           uchar_t; 
 typedef signed char             char_t;
 
-//Int Types
+/* Int Types */
 typedef signed long             int64_t;
 typedef unsigned long           uint64_t;
 
