4d61e2fae5594934738432d3ada31749456a797a
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-08 22:55:32 +0900

    必要そうなデータ型を作った

--- include/neox/types.h
+++ include/neox/types.h
@@ -2,21 +2,31 @@
 #define _TYPES_H
 
 /* True Or False */
-
 #define TRUE 1
 #define FALSE 0
 
-/* Bool Types */
-typedef unsigned char bool_t;
+/* NULL */
+typedef void NULL;
+NULL( char a, ... ) {
+    int a;
 
-/* Char Types */
-typedef unsigned char           uchar_t; 
-typedef signed char             char_t;
+};
 
-/* Int Types */
-typedef signed long             int64_t;
-typedef unsigned long           uint64_t;
 
+/* Boolean */
+typedef unsigned char bool;
+
+/* 符号付き整数 */
+typedef char i8_t 
+typedef short i16_t 
+typedef int i32_t 
+typedef long long i64_t 
+
+/* 符号なし整数 */
+typedef unsigned char u8_t
+typedef unsigned short u16_t
+typedef unsigned u32_t
+typedef unsigned long long u64_t
 
 
 #endif
