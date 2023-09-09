fbba341f1a74eb7b9146e73269bd9af958bcd79d
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-09 23:10:01 +0900

    Runtimeも追加

--- arch/uefi/include/efi.h
+++ arch/uefi/include/efi.h
@@ -222,6 +222,50 @@ typedef struct
     EFI_CREATE_EVENT_EX CreateEventEx; // UEFI 2.0+
 } EFI_BOOT_SERVICES;
 
+typedef struct {
+    EFI_TABLE_HEADER                 Hdr;
+
+    //
+    // Time Services
+    //
+    EFI_GET_TIME                     GetTime;
+    EFI_SET_TIME                     SetTime;
+    EFI_GET_WAKEUP_TIME              GetWakeupTime;
+    EFI_SET_WAKEUP_TIME              SetWakeupTime;
+
+    //
+    // Virtual Memory Services
+    //
+    EFI_SET_VIRTUAL_ADDRESS_MAP      SetVirtualAddressMap;
+  EFI_CONVERT_POINTER                ConvertPointer;
+
+    //
+    // Variable Services
+    //
+    EFI_GET_VARIABLE                 GetVariable;
+    EFI_GET_NEXT_VARIABLE_NAME       GetNextVariableName;
+    EFI_SET_VARIABLE                 SetVariable;
+
+
+    //
+    // Miscellaneous Services
+    //
+    EFI_GET_NEXT_HIGH_MONO_COUNT     GetNextHighMonotonicCount;
+    EFI_RESET_SYSTEM                 ResetSystem;
+
+   //
+   // UEFI 2.0 Capsule Services
+   //
+   EFI_UPDATE_CAPSULE               UpdateCapsule;
+   EFI_QUERY_CAPSULE_CAPABILITIES   QueryCapsuleCapabilities;
+
+
+ //
+ // Miscellaneous UEFI 2.0 Service
+ //
+  EFI_QUERY_VARIABLE_INFO          QueryVariableInfo;
+} EFI_RUNTIME_SERVICES;
+
 /* EFI_TIME */
 typedef struct
 {
