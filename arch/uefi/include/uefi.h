80ff2c219f3ea2cbcc1f65d144a66679b4b3c675
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-09 15:51:02 +0900

    includeの間違いが発生しないようにした

--- arch/uefi/include/uefi.h
+++ arch/uefi/include/uefi.h
@@ -1,8 +1,10 @@
 #ifndef _UEFI_H
 #define _UEFI_H
 
-#include "types.h"
-#include "protocol.h"
+
+#include "types.h" /* 常にブート処理の最初 */
+#include "struct.h" /* 常に二番目 */
+#include "protocol.h" /* 常に三番目 */
 
 
 #endif
