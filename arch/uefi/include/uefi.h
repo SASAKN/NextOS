92f66bdb785c29fd96f5ab13d12a3dc115cfa27e
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-09 15:47:40 +0900

    キーも移動

--- arch/uefi/include/uefi.h
+++ arch/uefi/include/uefi.h
@@ -4,7 +4,22 @@
 #include "types.h"
 #include "protocol.h"
 
+/* キーの入力 */
 
+/* EFI_INPUT_KEY */
+typedef struct
+{
+    UINT16 ScanCode;
+    UINT16 UnicodeChar;
+} EFI_INPUT_KEY;
+
+/* EFI_KEY_OPTION */
+typedef struct
+{
+    EFI_BOOT_KEY_DATA KeyData;
+    UINT32 BootOptionCrc;
+    UINT16 BootOption;
+} EFI_KEY_OPTION;
 
 /* メモリー */
 
