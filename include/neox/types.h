53f04d0661766e8512c9628237cc4be48f90a246
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-09 15:18:39 +0900

    これで、タイプを指定した

--- include/neox/types.h
+++ include/neox/types.h
@@ -12,16 +12,23 @@ typedef void NULL;
 typedef unsigned char bool;
 
 /* 符号付き整数 */
-typedef char i8_t;
-typedef short i16_t; 
-typedef int i32_t;
-typedef long long i64_t; 
+typedef char int8_t;
+typedef short int16_t; 
+typedef int int32_t;
+typedef long long int64_t; 
 
 /* 符号なし整数 */
-typedef unsigned char u8_t;
-typedef unsigned short u16_t;
-typedef unsigned u32_t;
-typedef unsigned long long u64_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned uint32_t;
+typedef unsigned long long uint64_t;
+
+/* Neoxが使うマクロ */
+
+/* Process */
+
+/* Process Identification */
+typedef 
 
 
 
