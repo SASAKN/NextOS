03e04a6f0fbce202d0c4c91a3d753c235a0fefc5
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-09 22:50:48 +0900

    GUIDなどを綺麗にフォーマット

--- arch/uefi/include/efi.h
+++ arch/uefi/include/efi.h
@@ -1,3 +1,21 @@
+#ifndef _EFI_H
+#define _EFI_H
+
+/* GUID */
+#define EFI_BOOT_MANAGER_POLICY_PROTOCOL_GUID              \
+    {                                                      \
+        0xFEDF8E0C, 0xE147, 0x11E3,                        \
+        {                                                  \
+            0x99, 0x03, 0xB8, 0xE8, 0x56, 0x2C, 0xBA, 0xFA \
+        }                                                  \
+    }
+#define EFI_DEVICE_PATH_PROTOCOL_GUID                      \
+    {                                                      \
+        0x09576e91, 0x6d3f, 0x11d2,                        \
+        {                                                  \
+            0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b \
+        }                                                  \
+    }
 /* EFI_BOOT_KEY_DATA */
 typedef union
 {
@@ -157,4 +175,4 @@ typedef struct
     EFI_CONFIGURATION_TABLE *ConfigurationTable;
 } EFI_SYSTEM_TABLE;
 
-
+#endif
--- arch/uefi/include/guid.h
+++ arch/uefi/include/guid.h
@@ -1,16 +1,4 @@
 #ifndef _ARCH_UEFI_GUID_H
 #define _ARCH_UEFI_GUID_H
 
-/* GUID */
-#define EFI_BOOT_MANAGER_POLICY_PROTOCOL_GUID              \
-    {                                                      \
-        0xFEDF8E0C, 0xE147, 0x11E3,                        \
-        {                                                  \
-            0x99, 0x03, 0xB8, 0xE8, 0x56, 0x2C, 0xBA, 0xFA \
-        }                                                  \
-    }
-#define EFI_DEVICE_PATH_PROTOCOL_GUID \
-  {0x09576e91,0x6d3f,0x11d2,\
-    {0x8e,0x39,0x00,0xa0,0xc9,0x69,0x72,0x3b}}
-
 #endif
