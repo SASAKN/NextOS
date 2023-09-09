93ced131e71a1ca6f4a3731439b48a7e187e80ca
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-09 15:26:16 +0900

    キーボード関係の移動

--- arch/uefi/include/types.h
+++ arch/uefi/include/types.h
@@ -58,39 +58,6 @@ typedef unsigned short CHAR16;
 /* VOID */
 typedef void VOID;
 
-/* EFI_INPUT_KEY */
-typedef struct
-{
-    UINT16 ScanCode;
-    UINT16 UnicodeChar;
-} EFI_INPUT_KEY;
-
-/* EFI_KEY_OPTION */
-typedef struct
-{
-    EFI_BOOT_KEY_DATA KeyData;
-    UINT32 BootOptionCrc;
-    UINT16 BootOption;
-} EFI_KEY_OPTION;
-
-/* EFI_BOOT_KEY_DATA */
-typedef union
-{
-    struct
-    {
-        UINT32 Revision : 8;
-        UINT32 ShiftPressed : 1;
-        UINT32 ControlPressed : 1;
-        UINT32 AltPressed : 1;
-        UINT32 LogoPressed : 1;
-        UINT32 MenuPressed : 1;
-        UINT32 SysReqPressed : 1;
-        UINT32 Reserved : 16;
-        UINT32 InputKeyCount : 2;
-    } Options;
-    UINT32 PackedValue;
-} EFI_BOOT_KEY_DATA;
-
 /* EFI_TIMER_DELAY */
 typedef enum
 {
