0d4ca20961d61dac078090efe1d74c8720d98a1f
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-09 15:20:56 +0900

    とりあえずおk

--- include/neox/types.h
+++ include/neox/types.h
@@ -23,6 +23,21 @@ typedef unsigned short uint16_t;
 typedef unsigned uint32_t;
 typedef unsigned long long uint64_t;
 
+
+/* 省略よう */
+/* 符号付き整数 */
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+/* 符号なし整数 */
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+
 /* Neoxが使うマクロ */
 
 /* Process */
