ec09ab0687012b8640b91dafea081aba570b9114
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-09 15:52:24 +0900

    ブート処理のルールを追加

--- arch/uefi/include/uefi.h
+++ arch/uefi/include/uefi.h
@@ -3,8 +3,9 @@
 
 
 #include "types.h" /* 常にブート処理の最初 */
-#include "struct.h" /* 常に二番目 */
-#include "protocol.h" /* 常に三番目 */
+#include "guid.h" /* 常に、二番目 */
+#include "struct.h" /* 常に三番目 */
+#include "protocol.h" /* 常に四番目 */
 
 
 #endif
