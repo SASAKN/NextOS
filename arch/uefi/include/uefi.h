a9d7d3e82e99bedea32614d4197a046512918ed1
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-09 15:55:30 +0900

    これで、UEFI.h読み込めば全部読み込まれる

--- arch/uefi/include/uefi.h
+++ arch/uefi/include/uefi.h
@@ -4,8 +4,9 @@
 
 #include "types.h" /* 常にブート処理の最初 */
 #include "guid.h" /* 常に、二番目 */
-#include "struct.h" /* 常に三番目 */
-#include "protocol.h" /* 常に四番目 */
+#include "systable.h" /* SYSTABLEは、自立できる */
+#include "struct.h" /* 常に4番目 */
+#include "protocol.h" /* 常に5番目 */
 
 
 #endif
