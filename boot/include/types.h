fe05ec34cb42c39d1e42df9d4ee585ba1c948d07
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-22 17:50:13 +0900

    データ型を書いた。

--- boot/include/types.h
+++ boot/include/types.h
@@ -1,8 +1,31 @@
 #ifndef _TYPES_H
 #define _TYPES_H
 
+/* IN OUT  何も意味は、ない*/
+#define IN
+#define OUT
+#define OPTIONAL
+
+/* Data Types */
 typedef unsigned char BOOLEAN;
-typedef long long int INTN;
-typedef unsigned long long int UINTN;
+typedef signed long long INTN;
+typedef unsigned long long UINTN;
+typedef signed char INT8;
+typedef unsigned char UINT8;
+typedef short INT16;
+typedef unsigned short UINT16;
+typedef int INT32;
+typedef unsigned int UINT32;
+typedef long long INT64;
+typedef unsigned long long UINT64;
+typedef char CHAR8;
+typedef unsigned short CHAR16;
+typedef void VOID;
+typedef unsigned long long EFI_STATUS;
+typedef VOID *EFI_HANDLE;
+typedef VOID *EFI_EVENT;
+typedef UINT64 EFI_LBA;
+typedef UINTN EFI_TPL;
+
 
 #endif
