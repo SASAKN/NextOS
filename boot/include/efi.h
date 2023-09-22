9237e573950325b230da18f14bfcd9a8985a1aae
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-22 17:44:42 +0900

    なんか余計なファイルが出ないようにした

---
+++ boot/include/efi.h
@@ -0,0 +1,13 @@
+/* UEFI 定義 */
+#ifndef _EFI_H
+#define _EFI_H
+
+typedef struct EFI_INPUT_KEY {
+    unsigned short ScanCode;
+    unsigned short UnicodeChar;
+};
+
+typedef struct EFI_GUID {
+}
+
+#endif
---
+++ boot/include/types.h
@@ -0,0 +1,8 @@
+#ifndef _TYPES_H
+#define _TYPES_H
+
+typedef unsigned char BOOLEAN;
+typedef long long int INTN;
+typedef unsigned long long int UINTN;
+
+#endif
