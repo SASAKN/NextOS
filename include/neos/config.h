063d9ed5ad5dd6c1772380d56eb62ec88a87b765
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-22 17:28:03 +0900

    ブートローダーの記述を追加

--- include/neos/config.h
+++ include/neos/config.h
@@ -4,13 +4,12 @@
 /* OS release and version numbers  */
 #define OS_NAME "neos"     /* OS Name */
 #define OS_CODENAME "none" /* OS Codename */
-#define OS_BOOTLOADER "" /* Currently only NeoBoot is supported */
+#define OS_BOOTLOADER "NEOBOOT" /* Currently only NeoBoot is supported */
 #define OS_RELEASE "1"     /* OS Version */
 #define OS_VERSION "1.0.0" /* OS Release */
 #define OS_DEVELOPER "SASAKEN" /* OS Developer */
 #define OS_EDITION "DEV" /* OS Editions (DEV, NONE, RELEASE) */
 #define OS_PLATFORM "ALL" /*OS PLATFORMS (UEFI_X64, ALL) */
-
 
 /* Machine Definitions */
 #include <neos/machine.h>
