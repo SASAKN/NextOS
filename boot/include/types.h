01348edf0f5222edb054c36baa3c03f2cbb026a5
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-23 17:46:22 +0900

    Clean Code

--- boot/include/types.h
+++ boot/include/types.h
@@ -40,12 +40,12 @@ typedef INT64 int64_t;
 
 /* ELF Types */
 typedef uintptr_t Elf64_Addr;
-typedef uint64_t Elf64_Off;
-typedef uint16_t Elf64_Half;
-typedef uint32_t Elf64_Word;
-typedef int32_t Elf64_Sword;
 typedef uint64_t Elf64_Xword;
+typedef uint64_t Elf64_Off;
+typedef uint32_t Elf64_Word;
+typedef uint16_t Elf64_Half;
 typedef int64_t Elf64_Sxword;
+typedef int32_t Elf64_Sword;
 
 /* ELF Macro */
 #define ELF_NIDENT 16
