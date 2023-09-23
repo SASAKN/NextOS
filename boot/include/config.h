2f2f0e97c82f040f212c27838413c83c2532a066
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-23 16:53:32 +0900

    ブートの設定をまとめておいた

---
+++ boot/include/config.h
@@ -0,0 +1,22 @@
+/* UEFI Config File */
+#ifndef _EFI_CONFIG_H
+#define _EFI_CONFIG_H
+
+#include "types.h" /* Must be First */
+
+/* Defines */
+
+/* File Name */
+#define KERNEL_FILE L"neos.elf"
+#define CONFIG_FILE L"config.boot"
+
+/* Directory Name */
+#define OS_APPS     L"apps/"
+#define OS_SERVER   L"servers/"
+#define OS_DRIVER   L"drivers/"
+
+/* Boot Setting */
+#define BOOT_DEVELOPER_MODE 1 /* Debug Mode */
+#define SPLASH              1 /* Boot Splash */  
+
+#endif
