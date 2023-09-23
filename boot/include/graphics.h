e0dee257dc7fcfd9b12b584224e6b9b3504f0280
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-23 17:30:50 +0900

    こんなふうに定義すればいいかな

--- boot/include/graphics.h
+++ boot/include/graphics.h
@@ -1,6 +1,9 @@
 #ifndef _FB_H
 #define _FB_H
 
+#include "types.h" /* Must be First. */
+
+/* FrameBuffer */
 struct frame_buffer {
     unsigned long long base;
     unsigned long long size;
@@ -8,9 +11,16 @@ struct frame_buffer {
     unsigned int vr;
 };
 
-extern struct frame_buffer frame_buffer;
+/* RECT */
+struct RECT {
+    UINT32 x, y;
+    UINT32 w, h;
+};
 
 /* Prototype */
 void init_frame_buffer(void);
+
+/* Extern */
+extern struct frame_buffer frame_buffer;
 
 #endif
