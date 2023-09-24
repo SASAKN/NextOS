ae9d6186ac41fb2e86e0397d8229529c8dc6a550
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-24 19:58:16 +0900

    Extern消してよくね

--- boot/boot.c
+++ boot/boot.c
@@ -1,2 +1,17 @@
 /* Neos Boot Loader v0.1 */
 
+#include "include/efi.h"
+#include "include/config.h"
+#include "include/elf.h"
+#include "include/graphics.h"
+#include "include/mem.h"
+
+/* Print Hello! */
+EFI_STATUS EFIAPI EfiMain(
+    IN EFI_HANDLE ImageHandle,
+    IN EFI_SYSTEM_TABLE *SystemTable)
+{
+    puts(L"Hello !");
+    while (1);
+    return EFI_SUCCESS;
+};
--- boot/run.sh
+++ boot/run.sh
@@ -2,4 +2,5 @@
 
 # NeoBoot Loader Build Tool 0.1
 
-
+clang -target x86_64-pc-win32-coff -mno-red-zone -fno-stack-protector -fshort-wchar -Wall -c boot.c
+lld-link /subsystem:efi_application /entry:EfiMain /out:boot.efi boot.o
