29d57c80a2f72e8830bb2586cb29241b6e70571c
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-24 11:11:14 +0900

    MEMORY管理用のファイルを追加

---
+++ boot/boot.c
@@ -0,0 +1,6 @@
+/* Neos Boot Loader v.0.1 */
+/* このブートローダーは、まだ非常に、簡易的なものであり、ELFの読み込みしかできません。 */
+
+#include "efi.h"
+
+EFI_STATUS GetMemoryMap(EFI_MEMO* map)
---
+++ boot/include/mem.h
@@ -0,0 +1,26 @@
+#ifndef _BOOT_MEM_H
+#define _BOOT_MEM_H
+
+#include "types.h" /* Must be First. */
+
+/* A memory map for booting */
+struct MemoryMap {
+    uint64_t buffer_size;
+    void* buffer;
+    uint64_t map_size;
+    uint64_t map_key;
+    uint64_t descriptor_size;
+    uint32_t descriptor_version;
+};
+
+/* EFI_MEMORY_DESCRIPTOR */
+struct MemoryDescriptor {
+    UINTN Type;
+    UINT32 PhysicalStart;
+    UINT32 VirtualStart;
+    UINT32 NumberOfPages;
+    UINT32 Attribute;
+};
+
+
+#endif
