600e156f7ae4438576650e2586318b37b9090916
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-24 22:12:16 +0900

    ウォッチドッグタイマーを０に設定

--- boot/efi/init.c
+++ boot/efi/init.c
@@ -5,4 +5,5 @@ EFI_SYSTEM_TABLE *ST;
 
 void efi_init(EFI_SYSTEM_TABLE *SystemTable) {
     ST = SystemTable;
+    ST->BootServices->SetWatchdogTimer(0, 0, 0, NULL); /* 5分何もしないでいると、再起動してしまうバグを修正 */
 };
