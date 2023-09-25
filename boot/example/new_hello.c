4fa6d13c46e2777a95e5afbc4598e98934016e98
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-25 22:49:31 +0900

    これで、関連付けできた

--- boot/example/new_hello.c
+++ boot/example/new_hello.c
@@ -1,4 +1,5 @@
 #include "../include/efi.h"
+#include "../include/proto.h" 
 #include "../include/elf.h"
 #include "../include/config.h"
 #include "../include/graphics.h"
@@ -8,7 +9,7 @@ EFI_STATUS EFIAPI EfiMain(
     IN EFI_HANDLE ImageHandle,
     IN EFI_SYSTEM_TABLE *SystemTable)
 {
-    puts
+    puts(L"Hello !");
     while (1);
     return EFI_SUCCESS;
 };
