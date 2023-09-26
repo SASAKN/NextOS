69b76dfa375827d76bf9eb93ecbe0f0d1a5c1292
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-26 22:35:05 +0900

    コメントを正しく付け替え

--- boot/boot.c
+++ boot/boot.c
@@ -12,7 +12,7 @@ EFI_STATUS EfiMain(
     IN EFI_HANDLE ImageHandle,
     IN EFI_SYSTEM_TABLE *SystemTable)
 {
-    efi_init(SystemTable); /* EFIのプロトコルなどを読み込む関数 */
+    efi_init(SystemTable); /* UEFIの全てを初期化する関数 */
     puts(L"Hello !");
     while (1);
     return EFI_SUCCESS;
