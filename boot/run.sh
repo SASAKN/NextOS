54353c33680e5a606d74fc5eb4986063351d7cba
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-26 22:27:05 +0900

    ビルドスクリプトも上手くかけた

--- boot/run.sh
+++ boot/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # NeoBoot Loader Build Tool 0.1
-
+ source /Users/kenta/.profile
 clang -target x86_64-pc-win32-coff -mno-red-zone -fno-stack-protector -fshort-wchar -Wall -c boot.c
 lld-link /subsystem:efi_application /entry:EfiMain /out:boot.efi boot.o
