2ef6e7cd91c6040f3c3a1d9bec36931873c60f52
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-26 22:14:21 +0900

    入力の取得もできるように

--- boot/common.c
+++ boot/common.c
@@ -12,6 +12,36 @@ void puts(UINT16 *s) {
     ST->ConOut->OutputString(ST->ConOut, s);
 };
 
+UINT16 getc(void) {
+    EFI_INPUT_KEY key;
+    UINT64 waitidx;
+
+    ST->BootServices->WaitForEvent(1, &(ST->ConIn->WaitForKey),
+                        &waitidx);
+    while (ST->ConIn->ReadKeyStroke(ST->ConIn, &key));
+    
+    return key.UnicodeChar;
+};
+
+UINT32 gets(UINT16 *buf, UINT32 buf_size) {
+    UINT32 i;
+
+    for(i = 0; i < buf_size - 1;) {
+        buf[i] = getc();
+        putc(buf[i]);
+        if (buf[i] == L'\r') {
+            putc(L'\n');
+            break;
+        };
+        i++;
+    };
+    buf[i] = L'\0';
+
+    return i;
+};
+
+
+
 UINT64 strlen (UINT16 *str) {
     UINT64 length = 0;
     /* ヌル文字になるまで、Lengthを増やしていく */
