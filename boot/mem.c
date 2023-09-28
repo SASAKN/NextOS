02971fd9cac289d9c35a10f496e8118c771c0dd2
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-28 22:12:28 +0900

    メモリー管理の準備２

---
+++ boot/mem.c
@@ -0,0 +1,12 @@
+/* Neos Boot Memory Manager v.0.1 */
+
+#include "include/types.h"
+#include "include/status.h"
+#include "include/config.h"
+#include "include/efi.h"
+#include "include/elf.h"
+#include "include/proto.h"
+#include "include/graphics.h"
+#include "include/mem.h"
+
+/* NEOSのブート中のメモリー管理 */
