59ef832f5522407222f5a938fb78091c5cfa409e
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-28 22:08:37 +0900

    メモリー関数が動作しなさそう

--- boot/boot.c
+++ boot/boot.c
@@ -7,32 +7,7 @@
 #include "include/graphics.h"
 #include "include/mem.h"
 
-/* メモリーマップをUEFIファームフェアからブートローダーへ受け渡す */
-EFI_STATUS GetMemoryMap(struct MemoryMap* mem_map)
-{
-    if (mem_map->buffer == NULL)
-    {
-        return EFI_BUFFER_TOO_SMALL; /* エラーを返す。 */
-    };
 
-    mem_map->map_size = mem_map->buffer_size;
-    return BS->GetMemoryMap(&mem_map->map_size, (EFI_MEMORY_DESCRIPTOR *)mem_map->buffer, &mem_map->map_key, &mem_map->descriptor_size, &mem_map->descriptor_version);
-};
-
-/* メモリーの詳細を画面に表示 */
-EFI_STATUS PrintMemoryMap(struct MemoryMap* mem_map) {
-    EFI_STATUS status;
-    CHAR8 buf[256];
-    UINTN len;
-    if(EFI_ERROR(status)) {
-        return status;
-    };
-
-    puts(L"mem_map->buffer = %08lx, mem_map->map_size = %08lx\n");
-    puts(mem_map->buffer);
-    puts(mem_map->map_size);
-
-}
 
 
 /* Entry Point ! */
@@ -43,7 +18,6 @@ EFI_STATUS EfiMain(
 {
     efi_init(SystemTable); /* UEFIの全てを初期化する関数 */
     puts(L"Hello !");
-    while (1)
-        ;
+    while (1);
     return EFI_SUCCESS;
 };
