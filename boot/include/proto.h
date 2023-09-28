c31bdba0e02f5411b9767b3dbd270286fac8ad3e
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-09-28 22:03:40 +0900

    バグを防ぐためにプロトタイプ関数を追加

--- boot/include/proto.h
+++ boot/include/proto.h
@@ -15,8 +15,10 @@ void *find_efi_acpi_table(void);
 void putc (UINT16 c);
 void puts(UINT16 *s);
 void Print(UINT16 *s); /* EDK2などに合わせるためにある関数 */
+void PrintHex(UINT64 val, UINT8 num_degits); /* 16進数の出力 */
 UINT16 getc(void);
 UINT32 gets(UINT16 *buf, UINT32 buf_size);
+
 /* Boot.c */
 EFI_STATUS GetMemoryMap(struct MemoryMap *map);
 
