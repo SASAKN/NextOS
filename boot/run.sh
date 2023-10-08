9d205e7e34cd4f923a79ce8aff151011c06d7b7a
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-10-08 20:57:13 +0900

    謎のインデントの削除

--- boot/run.sh
+++ boot/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # NeoBoot Loader Build Tool 0.1
- source /Users/kenta/.profile
- llvm
+source /Users/kenta/.profile
+llvm
 clang -target x86_64-pc-win32-coff -mno-red-zone -fno-stack-protector -fshort-wchar -Wall -c boot.c mem.c efi.c common.c
 lld-link /subsystem:efi_application /entry:EfiMain /out:boot.efi boot.o mem.o efi.o common.o
