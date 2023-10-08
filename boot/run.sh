1a172b06b9a26f0fa52a47cff6fc12c0691bbd52
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-10-08 21:56:10 +0900

    グラフィックなしで、起動できるように変更

--- boot/run.sh
+++ boot/run.sh
@@ -5,3 +5,5 @@ source /Users/kenta/.profile
 llvm
 clang -target x86_64-pc-win32-coff -mno-red-zone -fno-stack-protector -fshort-wchar -Wall -c boot.c mem.c efi.c common.c
 lld-link /subsystem:efi_application /entry:EfiMain /out:boot.efi boot.o mem.o efi.o common.o
+
+source make_image.sh
