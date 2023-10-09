7d4af262ba2ce4a415d4fd9ac5384f728e3d3841
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-10-09 17:26:57 +0900

    これで多分うまくいく

--- boot/boot.c
+++ boot/boot.c
@@ -16,10 +16,11 @@ EFI_STATUS EfiMain(
     IN EFI_HANDLE ImageHandle,
     IN EFI_SYSTEM_TABLE *SystemTable)
 {
-    struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
     efi_init(SystemTable); /* UEFIの全てを初期化する関数 */
+    CHAR8 memmap_buf[4096 * 4];
+    struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
     init_memmap(&mem_map);
-    // print_memmap(&mem_map);
+    print_memmap(&mem_map);
     while (1);
     return EFI_SUCCESS;
 };
