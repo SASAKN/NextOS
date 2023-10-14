529a9bf01d662756590a6a44a63461d84671f699
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-10-14 19:22:56 +0900

    テスト用のやつを削除

--- boot/boot.c
+++ boot/boot.c
@@ -14,10 +14,6 @@ EFI_STATUS EfiMain(
     IN EFI_SYSTEM_TABLE *SystemTable)
 {
     efi_init(SystemTable); /* UEFIの全てを初期化する関数 */
-    char buffer[100];
-    text_gen(buffer, sizeof(buffer), "%-ls", L"nemui");
-    custom_wprintf(L"%s\n", L"nemui");
-    custom_printf("[ INFO ]: %s\n", buffer);
     /* メモリーのバッファーなどを設定 */
     CHAR8 memmap_buf[MEM_DESC_SIZE];
     UINT64 memmap_size = MEM_DESC_SIZE;
