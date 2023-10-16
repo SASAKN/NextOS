2baceb6b4b187cf922d7eededd435c19f8b73c9b
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-10-16 22:01:02 +0900

    wprintfでまとめた

--- boot/boot.c
+++ boot/boot.c
@@ -7,15 +7,15 @@
 #include "include/graphics.h"
 #include "include/mem.h"
 
-EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL** root) {
-    EFI_LOADED_IMAGE_PROTOCOL* loaded_image;
-    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fs;
-    /* 例外の原因は、OpenProtocol */
-    // ST->BootServices->OpenProtocol(image_handle, &lip_guid, (VOID**)&fs, image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
-    // ST->BootServices->OpenProtocol(loaded_image->DeviceHandle, &sfsp_guid, (VOID**)&fs, image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
-    fs->OpenVolume(fs, root);
-    return EFI_SUCCESS;
-};
+// EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL** root) {
+//     EFI_LOADED_IMAGE_PROTOCOL* loaded_image;
+//     EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fs;
+//     /* 例外の原因は、OpenProtocol */
+//     // ST->BootServices->OpenProtocol(image_handle, &lip_guid, (VOID**)&fs, image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+//     // ST->BootServices->OpenProtocol(loaded_image->DeviceHandle, &sfsp_guid, (VOID**)&fs, image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+//     fs->OpenVolume(fs, root);
+//     return EFI_SUCCESS;
+// };
 
 
 /* Entry Point ! */
@@ -27,10 +27,9 @@ EFI_STATUS EfiMain(
     efi_init(SystemTable); /* UEFIの全てを初期化する関数 */
     custom_printf("Welcome to Neos !\n");
     EFI_LOADED_IMAGE_PROTOCOL *lip;
-    ST->BootServices->OpenProtocol(ImageHandle, &lip_guid, (VOID**)&lip, ImageHandle, NULL, EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+    UINT64 status = ST->BootServices->OpenProtocol(ImageHandle, &lip_guid, (VOID**)&lip, ImageHandle, NULL, EFI_OPEN_PROTOCOL_GET_PROTOCOL);
     assert(status, L"OpenProtocol");
-    puts(L"lip->FilePath :");
-    puts(DPTTP->ConvertDevicePathToText(lip->FilePath, FALSE, FALSE));
+    custom_wprintf(L"lip->FilePath : %s", DPTTP->ConvertDevicePathToText(lip->FilePath, FALSE, FALSE))
     puts(L"\r\n");
     // /* メモリーバッファー */
     // CHAR8 memmap_buf[MEM_DESC_SIZE];
