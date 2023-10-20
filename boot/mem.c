2121b334b8825d17c9d50272c26797cbc51ccdc1
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-10-20 23:12:57 +0900

    これでOK?

--- boot/mem.c
+++ boot/mem.c
@@ -75,8 +75,7 @@ void print_memmap(struct MemoryMap *map)
 		putc(L' ');
 		PrintHex(desc->Attribute, 16);
 		puts(L"\r\n");
-		text_gen(buffer, sizeof(buffer), "%u, %16x, %02lx, %-ls", i, (UINT64)desc, desc->Type, get_memtype_name(desc->Type));
-		text_gen(buffer, sizeof(buffer), "%16x, %16x, %16x, %16x \n", desc->PhysicalStart, desc->VirtualStart, desc->NumberOfPages, desc->Attribute);
+		text_gen(buffer, sizeof(buffer), "%u, %16x, %02lx, %-ls, %16x, %-ls, %16x, %-ls %16x, %-ls, %16x \n", i, (UINT64)desc, desc->Type, get_memtype_name(desc->Type), desc->PhysicalStart, L",", desc->VirtualStart, L",", desc->NumberOfPages, L"," desc->Attribute);
 		custom_printf("%s\n", buffer);
 		desc = (EFI_MEMORY_DESCRIPTOR *)((UINT8 *)desc + map->descriptor_size);
 	};
