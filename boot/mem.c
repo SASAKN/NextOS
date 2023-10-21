7c59f77210a9465fb64c36b17d5128f90a0e5717
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-10-21 13:49:41 +0900

    TEXT_GENを諦める

--- boot/mem.c
+++ boot/mem.c
@@ -58,7 +58,6 @@ void print_memmap(struct MemoryMap *map)
 	UINT16 *header = L"Index, Buffer, Type, Type(name),PhysicalStart, VirtualStart, NumberOfPages, Attribute\n";
 	puts(header);
 	putc(L' ');
-	char buffer[3000];
 	for (i = 0; i < map->memmap_desc_entry; i++)
 	{
 		PrintHex((unsigned long long)desc, 16);
@@ -75,8 +74,6 @@ void print_memmap(struct MemoryMap *map)
 		putc(L' ');
 		PrintHex(desc->Attribute, 16);
 		puts(L"\r\n");
-		text_gen(buffer, sizeof(buffer), "%u, %16x, %02lx, %-ls, %16x, %16x,%16x, %16x \n", i, (UINT64)desc, desc->Type, get_memtype_name(desc->Type), desc->PhysicalStart, desc->VirtualStart,  desc->NumberOfPages, desc->Attribute);
-		custom_printf("%s\n", buffer);
 		desc = (EFI_MEMORY_DESCRIPTOR *)((UINT8 *)desc + map->descriptor_size);
 	};
 };
