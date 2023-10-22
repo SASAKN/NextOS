aa0ce2047201017a136efbcff4dd511a99650f7b
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-10-22 18:11:14 +0900

    BSを追加

--- new_boot/include/efi.h
+++ new_boot/include/efi.h
@@ -4,34 +4,161 @@
 #include "types.h"
 
 /* EFI_H */
-typedef struct _EFI_LOAD_OPTION {
-  UINT32                            Attributes;
-  UINT16                            FilePathListLength;
+typedef struct _EFI_LOAD_OPTION
+{
+    UINT32 Attributes;
+    UINT16 FilePathListLength;
 } EFI_LOAD_OPTION;
 
-typedef struct _EFI_KEY_OPTION {
+typedef struct _EFI_KEY_OPTION
+{
     EFI_BOOT_KEY_DATA KeyData;
     UINT32 BootOptionCrc;
     UINT16 BootOption;
 } EFI_KEY_OPTION;
 
-typedef union {
-  struct {
-    UINT32   Revision : 8;
-    UINT32   ShiftPressed : 1;
-    UINT32   ControlPressed : 1;
-    UINT32   AltPressed : 1;
-    UINT32   LogoPressed : 1;
-    UINT32   MenuPressed : 1;
-    UINT32   SysReqPressed : 1;
-    UINT32   Reserved : 16;
-    UINT32   InputKeyCount : 2;
+typedef union
+{
+    struct
+    {
+        UINT32 Revision : 8;
+        UINT32 ShiftPressed : 1;
+        UINT32 ControlPressed : 1;
+        UINT32 AltPressed : 1;
+        UINT32 LogoPressed : 1;
+        UINT32 MenuPressed : 1;
+        UINT32 SysReqPressed : 1;
+        UINT32 Reserved : 16;
+        UINT32 InputKeyCount : 2;
     } Options;
-  UINT32 PackedValue;
+    UINT32 PackedValue;
 } EFI_BOOT_KEY_DATA;
 
+typedef EFI_STATUS(EFIAPI *EFI_IMAGE_ENTRY_POINT)(
+    IN EFI_HANDLE ImageHandle,
+    IN EFI_SYSTEM_TABLE *SystemTable);
 
+/* EFI_TABLE_HEADER */
+typedef struct
+{
+    UINT64 Signature;
+    UINT32 Revision;
+    UINT32 HeaderSize;
+    UINT32 CRC32;
+    UINT32 Reserved;
+} EFI_TABLE_HEADER;
 
+/* EFI_SYSTEM_TABLE */
+typedef struct
+{
+    EFI_TABLE_HEADER Hdr;
+    CHAR16 *FirmwareVendor;
+    UINT32 FirmwareRevision;
+    EFI_HANDLE ConsoleInHandle;
+    EFI_SIMPLE_TEXT_INPUT_PROTOCOL *ConIn;
+    EFI_HANDLE ConsoleOutHandle;
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
+    EFI_HANDLE StandardErrorHandle;
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *StdErr;
+    EFI_RUNTIME_SERVICES *RuntimeServices;
+    EFI_BOOT_SERVICES *BootServices;
+    UINTN NumberOfTableEntries;
+    EFI_CONFIGURATION_TABLE *ConfigurationTable;
+} EFI_SYSTEM_TABLE;
+
+/* EFI_BOOT_SERVICES */
+typedef struct
+{
+    EFI_TABLE_HEADER Hdr;
+
+    //
+    // Task Priority Services
+    //
+    EFI_RAISE_TPL RaiseTPL;     // EFI 1.0+
+    EFI_RESTORE_TPL RestoreTPL; // EFI 1.0+
+
+    //
+    // Memory Services
+    //
+    EFI_ALLOCATE_PAGES AllocatePages; // EFI 1.0+
+    EFI_FREE_PAGES FreePages;         // EFI 1.0+
+    EFI_GET_MEMORY_MAP GetMemoryMap;  // EFI 1.0+
+    EFI_ALLOCATE_POOL AllocatePool;   // EFI 1.0+
+    EFI_FREE_POOL FreePool;           // EFI 1.0+
+
+    //
+    // Event & Timer Services
+    //
+    EFI_CREATE_EVENT CreateEvent;    // EFI 1.0+
+    EFI_SET_TIMER SetTimer;          // EFI 1.0+
+    EFI_WAIT_FOR_EVENT WaitForEvent; // EFI 1.0+
+    EFI_SIGNAL_EVENT SignalEvent;    // EFI 1.0+
+    EFI_CLOSE_EVENT CloseEvent;      // EFI 1.0+
+    EFI_CHECK_EVENT CheckEvent;      // EFI 1.0+
+
+    //
+    // Protocol Handler Services
+    //
+    EFI_INSTALL_PROTOCOL_INTERFACE InstallProtocolInterface;     // EFI 1.0+
+    EFI_REINSTALL_PROTOCOL_INTERFACE ReinstallProtocolInterface; // EFI 1.0+
+    EFI_UNINSTALL_PROTOCOL_INTERFACE UninstallProtocolInterface; // EFI 1.0+
+    EFI_HANDLE_PROTOCOL HandleProtocol;                          // EFI 1.0+
+    VOID *Reserved;                                              // EFI 1.0+
+    EFI_REGISTER_PROTOCOL_NOTIFY RegisterProtocolNotify;         // EFI  1.0+
+    EFI_LOCATE_HANDLE LocateHandle;                              // EFI 1.0+
+    EFI_LOCATE_DEVICE_PATH LocateDevicePath;                     // EFI 1.0+
+    EFI_INSTALL_CONFIGURATION_TABLE InstallConfigurationTable;   // EFI 1.0+
+
+    //
+    // Image Services
+    //
+    EFI_IMAGE_UNLOAD LoadImage;              // EFI 1.0+
+    EFI_IMAGE_START StartImage;              // EFI 1.0+
+    EFI_EXIT Exit;                           // EFI 1.0+
+    EFI_IMAGE_UNLOAD UnloadImage;            // EFI 1.0+
+    EFI_EXIT_BOOT_SERVICES ExitBootServices; // EFI 1.0+
+
+    //
+    // Miscellaneous Services
+    //
+    EFI_GET_NEXT_MONOTONIC_COUNT GetNextMonotonicCount; // EFI 1.0+
+    EFI_STALL Stall;                                    // EFI 1.0+
+    EFI_SET_WATCHDOG_TIMER SetWatchdogTimer;            // EFI 1.0+
+
+    //
+    // DriverSupport Services
+    //
+    EFI_CONNECT_CONTROLLER ConnectController;       // EFI 1.1
+    EFI_DISCONNECT_CONTROLLER DisconnectController; // EFI 1.1+
+
+    //
+    // Open and Close Protocol Services
+    //
+    EFI_OPEN_PROTOCOL OpenProtocol;                        // EFI 1.1+
+    EFI_CLOSE_PROTOCOL CloseProtocol;                      // EFI 1.1+
+    EFI_OPEN_PROTOCOL_INFORMATION OpenProtocolInformation; // EFI 1.1+
+
+    //
+    // Library Services
+    //
+    EFI_PROTOCOLS_PER_HANDLE ProtocolsPerHandle;                                    // EFI 1.1+
+    EFI_LOCATE_HANDLE_BUFFER LocateHandleBuffer;                                    // EFI 1.1+
+    EFI_LOCATE_PROTOCOL LocateProtocol;                                             // EFI 1.1+
+    EFI_UNINSTALL_MULTIPLE_PROTOCOL_INTERFACES InstallMultipleProtocolInterfaces;   // EFI 1.1+
+    EFI_UNINSTALL_MULTIPLE_PROTOCOL_INTERFACES UninstallMultipleProtocolInterfaces; // EFI 1.1+*
+
+    //
+    // 32-bit CRC Services
+    //
+    EFI_CALCULATE_CRC32 CalculateCrc32; // EFI 1.1+
+
+    //
+    // Miscellaneous Services
+    //
+    EFI_COPY_MEM CopyMem;              // EFI 1.1+
+    EFI_SET_MEM SetMem;                // EFI 1.1+
+    EFI_CREATE_EVENT_EX CreateEventEx; // UEFI 2.0+
+} EFI_BOOT_SERVICES;
 
 
 #endif
