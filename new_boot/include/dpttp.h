02d62a640ca3944e96c79f5844fe54c11161626c
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-10-22 20:38:13 +0900

    DPTTPを追加

--- new_boot/include/dpttp.h
+++ new_boot/include/dpttp.h
@@ -1,7 +1,30 @@
 #ifndef _EFI_DPTTP_H
 #define _EFI_DPTTP_H
 
+#include "efi.h"
+
 /* DPTH_H */
+typedef
+CHAR16*
+(EFIAPI *EFI_DEVICE_PATH_TO_TEXT_NODE) (
+  IN CONST EFI_DEVICE_PATH_PROTOCOL*    DeviceNode,
+  IN BOOLEAN                    DisplayOnly,
+  IN BOOLEAN                    AllowShortcuts
+  );
+
+typedef
+CHAR16*
+(EFIAPI *EFI_DEVICE_PATH_TO_TEXT_PATH) (
+  IN CONST EFI_DEVICE_PATH_PROTOCOL       *DevicePath,
+  IN BOOLEAN                              DisplayOnly,
+  IN BOOLEAN                              AllowShortcuts
+  );
+
 /* DevicePathToTextProtocol */
+typedef struct _EFI_DEVICE_PATH_TO_TEXT_PROTOCOL {
+  EFI_DEVICE_PATH_TO_TEXT_NODE        ConvertDeviceNodeToText;
+  EFI_DEVICE_PATH_TO_TEXT_PATH        ConvertDevicePathToText;
+}  EFI_DEVICE_PATH_TO_TEXT_PROTOCOL;
+
 
 #endif
