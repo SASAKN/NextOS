fdb66973a8b8ca5570366066aa9c73fa0fd68684
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-10-22 18:13:07 +0900

    RTを追加

--- new_boot/include/efi.h
+++ new_boot/include/efi.h
@@ -17,6 +17,11 @@ typedef struct _EFI_KEY_OPTION
     UINT16 BootOption;
 } EFI_KEY_OPTION;
 
+typedef struct{
+  EFI_GUID           VendorGuid;
+  VOID               *VendorTable;
+}   EFI_CONFIGURATION_TABLE;
+
 typedef union
 {
     struct
@@ -159,6 +164,53 @@ typedef struct
     EFI_SET_MEM SetMem;                // EFI 1.1+
     EFI_CREATE_EVENT_EX CreateEventEx; // UEFI 2.0+
 } EFI_BOOT_SERVICES;
+
+/* EFI_RUNTIME_SERVICES */
+typedef struct {
+    EFI_TABLE_HEADER                 Hdr;
+
+    //
+    // Time Services
+    //
+    EFI_GET_TIME                     GetTime;
+    EFI_SET_TIME                     SetTime;
+    EFI_GET_WAKEUP_TIME              GetWakeupTime;
+    EFI_SET_WAKEUP_TIME              SetWakeupTime;
+
+    //
+    // Virtual Memory Services
+    //
+    EFI_SET_VIRTUAL_ADDRESS_MAP      SetVirtualAddressMap;
+  EFI_CONVERT_POINTER                ConvertPointer;
+
+    //
+    // Variable Services
+    //
+    EFI_GET_VARIABLE                 GetVariable;
+    EFI_GET_NEXT_VARIABLE_NAME       GetNextVariableName;
+    EFI_SET_VARIABLE                 SetVariable;
+
+
+    //
+    // Miscellaneous Services
+    //
+    EFI_GET_NEXT_HIGH_MONO_COUNT     GetNextHighMonotonicCount;
+    EFI_RESET_SYSTEM                 ResetSystem;
+
+   //
+   // UEFI 2.0 Capsule Services
+   //
+   EFI_UPDATE_CAPSULE               UpdateCapsule;
+   EFI_QUERY_CAPSULE_CAPABILITIES   QueryCapsuleCapabilities;
+
+
+ //
+ // Miscellaneous UEFI 2.0 Service
+ //
+  EFI_QUERY_VARIABLE_INFO          QueryVariableInfo;
+} EFI_RUNTIME_SERVICES;
+
+
 
 
 #endif
