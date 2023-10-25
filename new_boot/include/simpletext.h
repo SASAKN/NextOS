57fb4b984f8aed304beaa4a02d3ac1c4d7567056
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-10-25 22:11:41 +0900

    コメントアウト

--- new_boot/include/simpletext.h
+++ new_boot/include/simpletext.h
@@ -1,75 +1,77 @@
 #ifndef _EFI_SIMPLETEXT_H
 #define _EFI_SIMPLETEXT_H
 
-/* SimpleTextInputExProtocol */
+/* 新しいヘッダーに追加したものは、コメントアウト */
+
+// /* SimpleTextInputExProtocol */
 #include "efi.h"
 
-/* Guid */
-#define EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL_GUID             \
-    {                                                      \
-        0xdd9e7534, 0x7762, 0x4698,                        \
-        {                                                  \
-            0x8c, 0x14, 0xf5, 0x85, 0x17, 0xa6, 0x25, 0xaa \
-        }                                                  \
-    }
+// /* Guid */
+// #define EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL_GUID             \
+//     {                                                      \
+//         0xdd9e7534, 0x7762, 0x4698,                        \
+//         {                                                  \
+//             0x8c, 0x14, 0xf5, 0x85, 0x17, 0xa6, 0x25, 0xaa \
+//         }                                                  \
+//     }
 
-typedef struct _EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL
-{
-    EFI_INPUT_RESET_EX Reset;
-    EFI_INPUT_READ_KEY_EX ReadKeyStrokeEx;
-    EFI_EVENT WaitForKeyEx;
-    EFI_SET_STATE SetState;
-    EFI_REGISTER_KEYSTROKE_NOTIFY RegisterKeyNotify;
-    EFI_UNREGISTER_KEYSTROKE_NOTIFY UnregisterKeyNotify;
-} EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL;
+// typedef struct _EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL
+// {
+//     EFI_INPUT_RESET_EX Reset;
+//     EFI_INPUT_READ_KEY_EX ReadKeyStrokeEx;
+//     EFI_EVENT WaitForKeyEx;
+//     EFI_SET_STATE SetState;
+//     EFI_REGISTER_KEYSTROKE_NOTIFY RegisterKeyNotify;
+//     EFI_UNREGISTER_KEYSTROKE_NOTIFY UnregisterKeyNotify;
+// } EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL;
 
-typedef EFI_STATUS(EFIAPI *EFI_INPUT_READ_KEY_EX)(
-    IN struct _EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL *This,
-    OUT EFI_KEY_DATA *KeyData);
+// typedef EFI_STATUS(EFIAPI *EFI_INPUT_READ_KEY_EX)(
+//     IN struct _EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL *This,
+//     OUT EFI_KEY_DATA *KeyData);
 
-typedef EFI_STATUS(EFIAPI *EFI_SET_STATE)(
-    IN struct _EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL *This,
-    IN EFI_KEY_TOGGLE_STATE *KeyToggleState);
+// typedef EFI_STATUS(EFIAPI *EFI_SET_STATE)(
+//     IN struct _EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL *This,
+//     IN EFI_KEY_TOGGLE_STATE *KeyToggleState);
 
-typedef EFI_STATUS(EFIAPI *EFI_KEY_NOTIFY_FUNCTION)(
-    IN EFI_KEY_DATA *KeyData);
+// typedef EFI_STATUS(EFIAPI *EFI_KEY_NOTIFY_FUNCTION)(
+//     IN EFI_KEY_DATA *KeyData);
 
-typedef EFI_STATUS(EFIAPI *EFI_REGISTER_KEYSTROKE_NOTIFY)(
-    IN struct _EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL *This,
-    IN EFI_KEY_DATA *KeyData,
-    IN EFI_KEY_NOTIFY_FUNCTION KeyNotificationFunction,
-    OUT VOID **NotifyHandle);
+// typedef EFI_STATUS(EFIAPI *EFI_REGISTER_KEYSTROKE_NOTIFY)(
+//     IN struct _EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL *This,
+//     IN EFI_KEY_DATA *KeyData,
+//     IN EFI_KEY_NOTIFY_FUNCTION KeyNotificationFunction,
+//     OUT VOID **NotifyHandle);
 
-typedef EFI_STATUS(EFIAPI *EFI_UNREGISTER_KEYSTROKE_NOTIFY)(
-    IN struct _EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL *This,
-    IN VOID *NotificationHandle);
+// typedef EFI_STATUS(EFIAPI *EFI_UNREGISTER_KEYSTROKE_NOTIFY)(
+//     IN struct _EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL *This,
+//     IN VOID *NotificationHandle);
 
-/* SimpleTextInputProtocol */
+// /* SimpleTextInputProtocol */
 
-/* Guid */
-#define EFI_SIMPLE_TEXT_INPUT_PROTOCOL_GUID \
- {0x387477c1,0x69c7,0x11d2,\
-  {0x8e,0x39,0x00,0xa0,0xc9,0x69,0x72,0x3b}}
+// /* Guid */
+// #define EFI_SIMPLE_TEXT_INPUT_PROTOCOL_GUID \
+//  {0x387477c1,0x69c7,0x11d2,\
+//   {0x8e,0x39,0x00,0xa0,0xc9,0x69,0x72,0x3b}}
 
-typedef struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL {
- EFI_INPUT_RESET                       Reset;
- EFI_INPUT_READ_KEY                    ReadKeyStroke;
- EFI_EVENT                             WaitForKey;
-} EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
+// typedef struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL {
+//  EFI_INPUT_RESET                       Reset;
+//  EFI_INPUT_READ_KEY                    ReadKeyStroke;
+//  EFI_EVENT                             WaitForKey;
+// } EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
 
-typedef
-EFI_STATUS
-(EFIAPI *EFI_INPUT_RESET) (
- IN EFI_SIMPLE_TEXT_INPUT_PROTOCOL                 *This,
- IN BOOLEAN                                        ExtendedVerification
- );
+// typedef
+// EFI_STATUS
+// (EFIAPI *EFI_INPUT_RESET) (
+//  IN EFI_SIMPLE_TEXT_INPUT_PROTOCOL                 *This,
+//  IN BOOLEAN                                        ExtendedVerification
+//  );
 
- typedef
-EFI_STATUS
-(EFIAPI *EFI_INPUT_READ_KEY) (
- IN EFI_SIMPLE_TEXT_INPUT_PROTOCOL              *This,
- OUT EFI_INPUT_KEY                              *Key
- );
+//  typedef
+// EFI_STATUS
+// (EFIAPI *EFI_INPUT_READ_KEY) (
+//  IN EFI_SIMPLE_TEXT_INPUT_PROTOCOL              *This,
+//  OUT EFI_INPUT_KEY                              *Key
+//  );
 
 /* SimpleTextOutputProtocol */
 /* Guid */
