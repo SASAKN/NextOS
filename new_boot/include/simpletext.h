7bfa804b8e381b755d0c713456d3233e0b8863f7
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-10-25 22:24:38 +0900

    SimpleText関連は、完了

--- new_boot/include/simpletext.h
+++ new_boot/include/simpletext.h
@@ -80,83 +80,83 @@
   {0x8e,0x39,0x00,0xa0,0xc9,0x69,0x72,0x3b}}
 
 
- typedef
-EFI_STATUS
-(*EFI_TEXT_SET_CURSOR_POSITION) (
- IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
- IN UINTN                                       Column,
- IN UINTN                                       Row
- );
+//  typedef
+// EFI_STATUS
+// (*EFI_TEXT_SET_CURSOR_POSITION) (
+//  IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
+//  IN UINTN                                       Column,
+//  IN UINTN                                       Row
+//  );
 
-typedef
-EFI_STATUS
-(*EFI_TEXT_ENABLE_CURSOR) (
- IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
- IN BOOLEAN                                     Visible
- );
+// typedef
+// EFI_STATUS
+// (*EFI_TEXT_ENABLE_CURSOR) (
+//  IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
+//  IN BOOLEAN                                     Visible
+//  );
 
-typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
- EFI_TEXT_RESET                           Reset;
- EFI_TEXT_STRING                          OutputString;
- EFI_TEXT_TEST_STRING                     TestString;
- EFI_TEXT_QUERY_MODE                      QueryMode;
- EFI_TEXT_SET_MODE                        SetMode;
- EFI_TEXT_SET_ATTRIBUTE                   SetAttribute;
- EFI_TEXT_CLEAR_SCREEN                    ClearScreen;
- EFI_TEXT_SET_CURSOR_POSITION             SetCursorPosition;
- EFI_TEXT_ENABLE_CURSOR                   EnableCursor;
- SIMPLE_TEXT_OUTPUT_MODE                  *Mode;
-} EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+// typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
+//  EFI_TEXT_RESET                           Reset;
+//  EFI_TEXT_STRING                          OutputString;
+//  EFI_TEXT_TEST_STRING                     TestString;
+//  EFI_TEXT_QUERY_MODE                      QueryMode;
+//  EFI_TEXT_SET_MODE                        SetMode;
+//  EFI_TEXT_SET_ATTRIBUTE                   SetAttribute;
+//  EFI_TEXT_CLEAR_SCREEN                    ClearScreen;
+//  EFI_TEXT_SET_CURSOR_POSITION             SetCursorPosition;
+//  EFI_TEXT_ENABLE_CURSOR                   EnableCursor;
+//  SIMPLE_TEXT_OUTPUT_MODE                  *Mode;
+// } EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
-typedef
-EFI_STATUS
-(*EFI_TEXT_RESET) (
- IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
- IN BOOLEAN                               ExtendedVerification
- );
+// typedef
+// EFI_STATUS
+// (*EFI_TEXT_RESET) (
+//  IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
+//  IN BOOLEAN                               ExtendedVerification
+//  );
 
- typedef
-EFI_STATUS
-(*EFI_TEXT_STRING) (
- IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
- IN CHAR16                             *String
- );
+//  typedef
+// EFI_STATUS
+// (*EFI_TEXT_STRING) (
+//  IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
+//  IN CHAR16                             *String
+//  );
 
- typedef
-EFI_STATUS
-(*EFI_TEXT_TEST_STRING) (
- IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
- IN CHAR16                                *String
- );
+//  typedef
+// EFI_STATUS
+// (*EFI_TEXT_TEST_STRING) (
+//  IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
+//  IN CHAR16                                *String
+//  );
 
-typedef
-EFI_STATUS
-(*EFI_TEXT_QUERY_MODE) (
- IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL This,
- IN UINTN                                    ModeNumber,
- OUT UINTN                                   *Columns,
- OUT UINTN                                   *Rows
- );
+// typedef
+// EFI_STATUS
+// (*EFI_TEXT_QUERY_MODE) (
+//  IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL This,
+//  IN UINTN                                    ModeNumber,
+//  OUT UINTN                                   *Columns,
+//  OUT UINTN                                   *Rows
+//  );
 
-  typedef
-EFI_STATUS
-(*EFI_TEXT_SET_MODE) (
- IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
- IN UINTN                                    ModeNumber
- );
+//   typedef
+// EFI_STATUS
+// (*EFI_TEXT_SET_MODE) (
+//  IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
+//  IN UINTN                                    ModeNumber
+//  );
 
-typedef
-EFI_STATUS
-(*EFI_TEXT_SET_ATTRIBUTE) (
- IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
- IN UINTN                                  Attribute
- );
+// typedef
+// EFI_STATUS
+// (*EFI_TEXT_SET_ATTRIBUTE) (
+//  IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
+//  IN UINTN                                  Attribute
+//  );
 
- typedef
-EFI_STATUS
-(*EFI_TEXT_CLEAR_SCREEN) (
- IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This
- );
+//  typedef
+// EFI_STATUS
+// (*EFI_TEXT_CLEAR_SCREEN) (
+//  IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This
+//  );
 
 
 
