7f71f206d55120271a43828a14ee14b55f1591b8
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-10-27 18:15:23 +0900

    GOPを追加

--- neo_boot/include/efi.h
+++ neo_boot/include/efi.h
@@ -79,15 +79,6 @@ typedef struct _EFI_KEY_DATA
     EFI_KEY_STATE KeyState;
 } EFI_KEY_DATA;
 
-typedef enum _EFI_GRAPHIICS_PIXEL_FORMAT
-{
-    PixelRedGreenBlueReserved8BitPerColor,
-    PixelBlueGreenRedReserved8BitPerColor,
-    PixelBitMask,
-    PixelBltOnly,
-    PixelFormatMax
-} EFI_GRAPHICS_PIXEL_FORMAT;
-
 typedef enum _EFI_LOCATE_SEARCH_TYPE
 {
     AllHandles,
@@ -371,7 +362,8 @@ typedef _EFI_BOOT_SERVICES
     EFI_COPY_MEM CopyMem;              // EFI 1.1+
     EFI_SET_MEM SetMem;                // EFI 1.1+
     EFI_CREATE_EVENT_EX CreateEventEx; // UEFI 2.0+
-} EFI_BOOT_SERVICES;
+}
+EFI_BOOT_SERVICES;
 
 /* @End BootServices */
 
@@ -416,6 +408,90 @@ typedef struct
 /* @End SystemTable */
 
 /* GraphicsOutputProtocol */
+
+typedef enum _EFI_GRAPHIICS_PIXEL_FORMAT
+{
+    PixelRedGreenBlueReserved8BitPerColor,
+    PixelBlueGreenRedReserved8BitPerColor,
+    PixelBitMask,
+    PixelBltOnly,
+    PixelFormatMax
+} EFI_GRAPHICS_PIXEL_FORMAT;
+
+typedef struct
+{
+    UINT32 RedMask;
+    UINT32 GreenMask;
+    UINT32 BlueMask;
+    UINT32 ReservedMask;
+} EFI_PIXEL_BITMASK;
+
+typedef struct
+{
+    UINT32 Version;
+    UINT32 HorizontalResolution;
+    UINT32 VerticalResolution;
+    EFI_GRAPHICS_PIXEL_FORMAT PixelFormat;
+    EFI_PIXEL_BITMASK PixelInformation;
+    UINT32 PixelsPerScanLine;
+} EFI_GRAPHICS_OUTPUT_MODE_INFORMATION;
+
+typedef struct
+{
+    UINT32 MaxMode;
+    UINT32 Mode;
+    EFI_GRAPHICS_OUTPUT_MODE_INFORMATION *Info;
+    UINTN SizeOfInfo;
+    EFI_PHYSICAL_ADDRESS FrameBufferBase;
+    UINTN FrameBufferSize;
+} EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE;
+
+typedef struct
+{
+    UINT8 Blue;
+    UINT8 Green;
+    UINT8 Red;
+    UINT8 Reserved;
+} EFI_GRAPHICS_OUTPUT_BLT_PIXEL;
+
+typedef enum
+{
+    EfiBltVideoFill,
+    EfiBltVideoToBltBuffer,
+    EfiBltBufferToVideo,
+    EfiBltVideoToVideo,
+    EfiGraphicsOutputBltOperationMax
+} EFI_GRAPHICS_OUTPUT_BLT_OPERATION;
+
+typedef struct EFI_GRAPHICS_OUTPUT_PROTCOL
+{
+    EFI_STATUS (*QueryMode)
+    (
+        IN EFI_GRAPHICS_OUTPUT_PROTOCOL *This,
+        IN UINT32 ModeNumber,
+        OUT UINTN *SizeOfInfo
+            OUT EFI_GRAPHICS_OUTPUT_MODE_INFORMATION **Info);
+    EFI_STATUS (*SetMode)
+    (
+        IN EFI_GRAPHICS_OUTPUT_PROTOCOL *This,
+        IN UINT32 ModeNumber);
+    EFI_STATUS(*Blt)
+    (
+        IN EFI_GRAPHICS_OUTPUT_PROTOCOL *This,
+        IN OUT EFI_GRAPHICS_OUTPUT_BLT_PIXEL *BltBuffer, OPTIONAL IN EFI_GRAPHICS_OUTPUT_BLT_OPERATION BltOperation,
+        IN UINTN SourceX,
+        IN UINTN SourceY,
+        IN UINTN DestinationX,
+        IN UINTN DestinationY,
+        IN UINTN Width,
+        IN UINTN Height,
+        IN UINTN Delta OPTIONAL);
+    EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE *Mode;
+} EFI_GRAPHICS_OUTPUT_PROTOCOL;
+
+/* @End GraphicsOutputProtocol */
+
+
 
 typedef struct
 {
