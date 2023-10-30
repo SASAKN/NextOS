795bd70dd3a79d7fea5a7f080e52e0225513d4ce
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-10-30 16:41:26 +0900

    Update run_image.h

--- neo_boot/run_image.sh
+++ neo_boot/run_image.sh
@@ -5,12 +5,12 @@ script_dir="$(dirname "$(readlink -f "$0")")"
 #    -nographic -serial mon:stdio
 
 qemu-system-x86_64 \
-    -nographic -serial mon:stdio \
-    -m 3G \
+    -m 4G \
     -drive if=pflash,format=raw,file=${script_dir}/OVMF.fd \
     -drive if=ide,index=0,media=disk,format=raw,file=${script_dir}/neos.img \
     -device nec-usb-xhci,id=xhci \
-    -device usb-mouse -device usb-kbd 
+    -device usb-mouse -device usb-kbd \
+    -monitor stdio
 
 # -monitor stdio　//GUIを使いたい場合
 
