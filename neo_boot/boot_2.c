011ffef207c3fe4243a46b789dfc63f921078cfe
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-02 21:00:30 +0900

    とりあえず、BOOT処理を一回やめる

--- neo_boot/boot_2.c
+++ neo_boot/boot_2.c
@@ -129,14 +129,14 @@ EFI_STATUS EfiMain(
   /* ブートローダーから離脱 */
   ExitBootLoader(ImageHandle, &map);
   PrintOK(SystemTable);
-  custom_printf("Welcome to NEOS !\n");
+  custom_printf("ExitBootServices\n");
   /* カーネルの読み出し */
-  UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
+  UINT64 entry_addr = 0x100018; //AllocateAddress
   char buf_final[200];
   text_gen(buf_final, sizeof(buf_final), "0x%x", entry_addr);
   custom_printf("NEOS Kernel Entry Point Address :%s\n", buf_final);
   typedef void EntryPointType(void);
-  EntryPointType* entry_point = (EntryPointType *)kernel_base_addr;
+  EntryPointType* entry_point = (EntryPointType *)entry_addr;
   entry_point();
   while (1);
   return EFI_SUCCESS;
