574450f375ed5e0e672188f6541148506edde7ee
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-04 17:32:52 +0900

    なんかおかしいところを修正

--- neo_boot/build.sh
+++ neo_boot/build.sh
@@ -3,4 +3,4 @@
 source /Users/kenta/.profile
 llvm
 clang -target x86_64-pc-win32-coff -mno-red-zone -mno-stack-arg-probe -fno-stack-protector -fshort-wchar -Wall -c boot.c mem.c efi.c common.c
-lld-link /subsystem:efi_application /entry:EfiMain /out:boot.efi boot.o mem.o efi.o common.o kernel.o
+lld-link /subsystem:efi_application /entry:EfiMain /out:boot.efi boot.o mem.o efi.o common.o
