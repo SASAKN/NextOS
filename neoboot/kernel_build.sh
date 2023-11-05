661dbdba8875ab5f84388effc7660fb4f0fa1bd8
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-05 13:40:26 +0900

    ビルドツールとカーネル

---
+++ neoboot/kernel_build.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+#Copyright SASAKEN
+#For Test.
+source /Users/kenta/.profile
+llvm
+clang -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -c test_kernel.c
+ld.lld --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf test_kernel.o
