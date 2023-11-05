efe2d8a5267a01ffcf934ea0fe1e2f740029d03a
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-05 16:20:49 +0900

    やっぱEDK2だるい

--- NeoBootPkg/neoboot.c
+++ NeoBootPkg/neoboot.c
@@ -1,10 +1,22 @@
-// NeoBoot v.0.01 Copyright SASAKEN
+// neoboot v.0.01
+// neoboootは、elfを読み込み、カーネルに、neobootの構造体を送ります。
+
 
 /* EDK2で必要なファイルのinclude */
-
+#include  <Uefi.h>
+#include  <Library/UefiLib.h>
+#include  <Library/UefiBootServicesTableLib.h>
+#include  <Library/PrintLib.h>
+#include  <Protocol/LoadedImage.h>
+#include  <Protocol/SimpleFileSystem.h>
+#include  <Protocol/DiskIo2.h>
+#include  <Protocol/BlockIo.h>
+#include  <Guid/FileInfo.h>
 
 /* 独自Includeファイル */
 #include <table.h>
 #include <mem.h>
 #include <graphics.h>
+
+
 
