017bd48169f1a74781233055a87a45fe26a6a21f
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-05 11:07:03 +0900

    EDK2っぽく変更

--- neoboot/base.h
+++ neoboot/base.h
@@ -2,4 +2,12 @@
 #ifndef _BOOT_BASE_H
 #define _BOOT_BASE_H
 
+#include "include/efi.h"
+
+/* Boot Base */
+/* これは、NEOBOOTの初期化のためのものです。 */
+extern EFI_SYSTEM_TABLE *gST;
+extern EFI_BOOT_SERVICES *gBS;
+extern EFI_RUNTIME_SERVICES *gRT;
+
 #endif
