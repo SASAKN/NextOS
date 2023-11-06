8cb8910af1d05199da7062612ab786b98192f139
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-06 22:59:14 +0900

    run_imageするように変更

---
+++ neoboot/run_image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+script_dir="$(dirname "$(readlink -f "$0")")"
+
+#    -nographic -serial mon:stdio
+
+qemu-system-x86_64 \
+    -nographic -serial mon:stdio \
+    -m 3G \
+    -drive if=pflash,format=raw,file=${script_dir}/OVMF.fd \
+    -drive if=ide,index=0,media=disk,format=raw,file=${script_dir}/neos.img \
+    -device nec-usb-xhci,id=xhci \
+    -device usb-mouse -device usb-kbd 
+
+# -monitor stdio　//GUIを使いたい場合
+
+#GUIを使用していないときに、終了したい時はctrl a xで、終了できます
