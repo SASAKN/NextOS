3289105cf6f152526825e47076c863410927fd6a
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-06 23:10:35 +0900

    このライブラリー使いやすい

--- neoboot/neoboot.c
+++ neoboot/neoboot.c
@@ -2,5 +2,8 @@
 
 int main( int argc, char **argv ) {
     printf("Hello World!\n");
-    __asm__ volatile("hlt");
+    printf("Boot Success ! (hlt)\n");
+    /* hltないと処理が進んでしまう */
+    while (1) __asm__ ("hlt");
+    return EFI_SUCCESS;
 }
