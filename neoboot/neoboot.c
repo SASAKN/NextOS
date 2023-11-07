5523b8730d9db6f06591ae931d94b384930c14f7
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-07 23:30:21 +0900

    これでどうだろう

--- neoboot/neoboot.c
+++ neoboot/neoboot.c
@@ -1,10 +1,91 @@
 #include "uefi/uefi.h"
 #include "include/mem.h"
 
+char_t *get_memtype_name(efi_memory_type_t type)
+{
+	switch (type)
+	{
+	case EfiReservedMemoryType:
+		return "EfiReservedMemoryType";
+	case EfiLoaderCode:
+		return "EfiLoaderCode";
+	case EfiLoaderData:
+		return "EfiLoaderData";
+	case EfiBootServicesCode:
+		return "EfiBootServicesCode";
+	case EfiBootServicesData:
+		return "EfiBootServicesData";
+	case EfiRuntimeServicesCode:
+		return "EfiRuntimeServicesCode";
+	case EfiRuntimeServicesData:
+		return "EfiRuntimeServicesData";
+	case EfiConventionalMemory:
+		return "EfiConventionalMemory";
+	case EfiUnusableMemory:
+		return "EfiUnusableMemory";
+	case EfiACPIReclaimMemory:
+		return "EfiACPIReclaimMemory";
+	case EfiACPIMemoryNVS:
+		return "EfiACPIMemoryNVS";
+	case EfiMemoryMappedIO:
+		return "EfiMemoryMappedIO";
+	case EfiMemoryMappedIOPortSpace:
+		return "EfiMemoryMappedIOPortSpace";
+	case EfiPalCode:
+		return "EfiPalCode";
+	case EfiPersistentMemory:
+		return "EfiPersistentMemory";
+	case EfiMaxMemoryType:
+		return "EfiMaxMemoryType";
+	default:
+		return "InvalidMemoryType";
+	}
+};
+
 int main( int argc, char **argv ) {
     //Init UEFI Lib.
     (void)argc;
     (void)argv;
     efi_status_t status;
+    printf("Welcome to Neo Boot !");
+    struct MemoryMap map;
+    map.map_size = 0;
+    map.map_key = 0;
+    map.descriptor_size = 0;
+    //構造体に必要なものを１回目で取得
+    status = BS->GetMemoryMap(&map.map_size, NULL, &map.map_key, &map.descriptor_size, NULL);
+    //エラー処理
+    if (status != EFI_BUFFER_TOO_SMALL || !map.map_size) {
+        fprintf(stderr, "Unable to Get Memory Map !\n");
+        while(1) __asm__ ("hlt"); 
+        return 0;
+    }
+    //メモリーマップのサイズの処理
+    map.map_size += 4 * map.descriptor_size;
+    char_t memmap_buf[map.map_size];
+    if (!map.map_size) {
+        fprintf(stderr, "unable to allocate memory. \n");
+        while(1) __asm__ ("hlt"); 
+        return 1;
+    }
+    // 2回目の実行で取得 
+    status = BS->GetMemoryMap(&map.map_size, (efi_memory_descriptor_t*)map.buffer, &map.map_key, &map.descriptor_size, NULL);
+    if (EFI_ERROR(status)) {
+        fprintf(stderr, "Unable to Get Memory Map !\n");
+        while(1) __asm__ ("hlt"); 
+        return 0;
+    }
+    map.memmap_desc_entry = map.map_size / map.descriptor_size;
+    printf("[ INFO ] MemoryMap\n");
+    printf("Index, Buffer, Type, Type(name),PhysicalStart, VirtualStart, NumberOfPages, Attribute\n");
+    uint32_t i;
+    efi_memory_descriptor_t *desc = (efi_memory_descriptor_t *)map.buffer;
+    for (i = 0; i < map.memmap_desc_entry; i++) {
+        printf("%02x, %016x, %02x, %s, %016x, %016x, %016x, %016x \r\n", i, desc, desc->Type, get_memtype_name(desc->Type), desc->PhysicalStart, desc->VirtualStart, desc->NumberOfPages, desc->Attribute);
+        desc = (efi_memory_descriptor_t *)((uint8_t *)desc + map.descriptor_size);
+    }
 
+    // halt cpu.
+    while(1) __asm__ ("hlt"); 
+    return EFI_SUCCESS;
 }
