767d3f2859cbec6faacbe7b82a6e31ff955eca82
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-07 22:29:50 +0900

    コメントをしっかり追加

--- neoboot/neoboot.c
+++ neoboot/neoboot.c
@@ -3,7 +3,7 @@
 int main( int argc, char **argv ) {
     printf("Hello World!\n");
     printf("Boot Success ! (hlt)\n");
-    /* hltないと処理が進んでしまう */
+    // hltがないと処理が進んでしまう
     while (1) __asm__ ("hlt");
     return EFI_SUCCESS;
 }
