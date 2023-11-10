6ccfba0ab3f3b50ed44ac295f40b3692ff95cee4
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-10 17:55:25 +0900

    スプラッシュを諦める

--- neoboot/neoboot.c
+++ neoboot/neoboot.c
@@ -1,8 +1,5 @@
 #include "uefi/uefi.h"
 #include "include/mem.h"
-// STB IMAGE LIBRARY
-#define STB_IMAGE_IMPLEMENTATION
-#include "include/stb_image.h"
 
 void PrintOK(void)
 {
@@ -28,21 +25,21 @@ void PrintError(void)
 /* ベンダーなどの情報を表示 */
 void PrintEfiConfigurationTable(void)
 {
-    uint64_t i;
-    printf("\n[ INFO ] EfiConfigurationTable\n");
-    for (i = 0; i < ST->NumberOfTableEntries; i++)
-    {
-        printf("%02d : %016x : %08x, %04x, %04x\n", i, (unsigned long long)&ST->ConfigurationTable[i], ST->ConfigurationTable[i].VendorGuid.Data1, ST->ConfigurationTable[i].VendorGuid.Data2, ST->ConfigurationTable[i].VendorGuid.Data3);
-        unsigned char j;
-        for (j = 0; j < 8; j++)
-        {
-            printf("%02x", ST->ConfigurationTable[i].VendorGuid.Data4[j]);
-        }
-        printf("%016x\n", (unsigned long long)ST->ConfigurationTable[i].VendorTable);
+  uint64_t i;
+  printf("\n[ INFO ] EfiConfigurationTable\n");
+  for (i = 0; i < ST->NumberOfTableEntries; i++)
+  {
+    printf("%02d : %016x : %08x, %04x, %04x\n", i, (unsigned long long)&ST->ConfigurationTable[i], ST->ConfigurationTable[i].VendorGuid.Data1, ST->ConfigurationTable[i].VendorGuid.Data2, ST->ConfigurationTable[i].VendorGuid.Data3);
+    unsigned char j;
+    for (j = 0; j < 8; j++) {
+      printf("%02x" , ST->ConfigurationTable[i].VendorGuid.Data4[j]);
     }
-    PrintOK();
-    printf("VendorTable\n");
+    printf("%016x\n", (unsigned long long)ST->ConfigurationTable[i].VendorTable);
+  }
+  PrintOK();
+  printf("VendorTable\n");
 };
+
 
 char_t *get_memtype_name(efi_memory_type_t type)
 {
@@ -152,7 +149,7 @@ efi_status_t save_memmap(struct MemoryMap *map)
     efi_memory_descriptor_t *desc = (efi_memory_descriptor_t *)map->buffer;
     if ((f = fopen("\\memmap", "a")))
     {
-        // ヘッダーの書き込み
+        //ヘッダーの書き込み
         fprintf(f, "%s", header);
         for (uint32_t i = 0; i < map->memmap_desc_entry; i++)
         {
@@ -166,20 +163,17 @@ efi_status_t save_memmap(struct MemoryMap *map)
     return 0;
 }
 
-efi_status_t test_memmap_file(void)
-{
+efi_status_t test_memmap_file(void) {
     FILE *f;
     char_t *buff;
     size_t size;
-    if ((f = fopen("\\memmap", "r")))
-    {
+    if ((f = fopen("\\memmap", "r"))) {
         fseek(f, 0, SEEK_END);
         size = ftell(f);
         fseek(f, 0, SEEK_SET);
         printf("[ INFO ] Memory Map File Size : %d bytes.\n", size);
         buff = malloc(size + 1);
-        if (!buff)
-        {
+        if (!buff) {
             PrintError();
             fprintf(stderr, "unable to allocate memory.\n");
             return 1;
@@ -188,15 +182,12 @@ efi_status_t test_memmap_file(void)
         buff[size] = 0;
         fclose(f);
         // printf("[T]:\n%s\n", buff);
-        if (size <= 94)
-        {
+        if (size <= 94) {
             PrintError();
             printf("Save File\n");
         }
         free(buff);
-    }
-    else
-    {
+    } else {
         PrintError();
         fprintf(stderr, "unable to open file\n");
         return 0;
@@ -204,92 +195,7 @@ efi_status_t test_memmap_file(void)
     PrintOK();
     printf("Read Memory Map file\n");
     return 0;
-}
 
-efi_status_t load_splash(int32_t w, int32_t h, int32_t l, uint32_t *data, unsigned char *buffer)
-{
-    FILE *f;
-    long int size;
-    stbi__context s;
-    stbi__result_info ri;
-
-    if ((f = fopen("\\splash.png", "r")))
-    {
-        // Read Size.
-        fseek(f, 0, SEEK_END);
-        size = ftell(f);
-        fseek(f, 0, SEEK_SET);
-        // Allocate Buffer
-        buffer = (unsigned char *)malloc(size);
-        if (!buffer)
-        {
-            PrintError();
-            fprintf(stderr, "Unable to allocate memory.\n");
-        }
-        // Read File
-        fread(buffer, size, 1, f);
-        // Close File
-        fclose(f);
-        // Decode png image
-        ri.bits_per_channel = 8;
-        s.read_from_callbacks = 0;
-        s.img_buffer = s.img_buffer_original = buffer;
-        s.img_buffer_end = s.img_buffer_original_end = buffer + size;
-        data = (uint32_t *)stbi__png_load(&s, &w, &h, &l, 4, &ri);
-        if (!data)
-        {
-            PrintError();
-            fprintf(stdout, "Unable to decode png : %s\n", stbi__g_failure_reason);
-            return EFI_SUCCESS;
-        }
-    }
-    else
-    {
-        PrintOK();
-        printf("No Splash in folder with bootloader\n");
-    }
-    return EFI_SUCCESS;
-}
-
-efi_status_t show_splash(efi_gop_t *gop, efi_guid_t *gop_guid, int32_t w, int32_t h, int32_t l, uint32_t *data, unsigned char *buffer)
-{
-    // set video mode
-    efi_status_t status;
-    status = BS->LocateProtocol(gop_guid, NULL, (void **)&gop);
-    if (!EFI_ERROR(status) && gop)
-    {
-        status = gop->SetMode(gop, 0);
-        // ST->ConOut->Reset(ST->ConOut, 0);
-        // ST->StdErr->Reset(ST->StdErr, 0);
-        if (EFI_ERROR(status))
-        {
-            PrintError();
-            fprintf(stderr, "unable to set video mode\n");
-            return 0;
-        }
-    }
-    else
-    {
-        PrintError();
-        fprintf(stderr, "unable to get graphics output protocol\n");
-        return 0;
-    }
-
-    // RGBA to BGRA
-    if (gop->Mode->Information->PixelFormat == PixelBlueGreenRedReserved8BitPerColor || (gop->Mode->Information->PixelFormat == PixelBitMask && gop->Mode->Information->PixelInformation.BlueMask != 0xff0000))
-    {
-        for (l = 0; l < w * h; l++)
-        {
-            data[l] = ((data[l] & 0xff) << 16) | (data[l] & 0xff00) | ((data[l] >> 16) & 0xff);
-        }
-    }
-
-    // Display Splash
-    gop->Blt(gop, data, EfiBltBufferToVideo, 0, 0, (gop->Mode->Information->HorizontalResolution - w) / 2,
-             (gop->Mode->Information->VerticalResolution - h) / 2, w, h, 0);
-    free(data);
-    free(buffer);
-    return EFI_SUCCESS;
 }
 
 int main(int argc, char **argv)
@@ -298,22 +204,9 @@ int main(int argc, char **argv)
     (void)argc;
     (void)argv;
     efi_status_t status;
-    // Graphics
-    efi_guid_t gopGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
-    efi_gop_t *gop = NULL;
-    FILE *f;
-    unsigned char *buff;
-    uint32_t *data;
-    int w, h, l;
-    long int size;
-    stbi__context s;
-    stbi__result_info ri;
-    // Reset Watchdog Timer
     ST->BootServices->SetWatchdogTimer(0, 0, 0, NULL);
-    // Start
     PrintOK();
     printf("Welcome to Neo Boot !\n");
-    // EfiConfigurationTable
     PrintEfiConfigurationTable();
     // MemoryMap
     struct MemoryMap map;
@@ -325,62 +218,6 @@ int main(int argc, char **argv)
     print_memmap(&map);
     save_memmap(&map);
     test_memmap_file();
-    // Splash
-     /* load image */
-    if((f = fopen("\\splash.png", "r"))) {
-        fseek(f, 0, SEEK_END);
-        size = ftell(f);
-        fseek(f, 0, SEEK_SET);
-        buff = (unsigned char*)malloc(size);
-        if(!buff) {
-            fprintf(stderr, "unable to allocate memory\n");
-            return 1;
-        }
-        fread(buff, size, 1, f);
-        fclose(f);
-        ri.bits_per_channel = 8;
-        s.read_from_callbacks = 0;
-        s.img_buffer = s.img_buffer_original = buff;
-        s.img_buffer_end = s.img_buffer_original_end = buff + size;
-        data = (uint32_t*)stbi__png_load(&s, &w, &h, &l, 4, &ri);
-        if(!data) {
-            fprintf(stdout, "Unable to decode png: %s\n", stbi__g_failure_reason);
-            return 0;
-        }
-    } else {
-        fprintf(stderr, "Unable to load image\n");
-        return 0;
-    }
-
-    /* set video mode */
-    status = BS->LocateProtocol(&gopGuid, NULL, (void**)&gop);
-    if(!EFI_ERROR(status) && gop) {
-        status = gop->SetMode(gop, 0);
-        ST->ConOut->Reset(ST->ConOut, 0);
-        ST->StdErr->Reset(ST->StdErr, 0);
-        if(EFI_ERROR(status)) {
-            fprintf(stderr, "unable to set video mode\n");
-            return 0;
-        }
-    } else {
-        fprintf(stderr, "unable to get graphics output protocol\n");
-        return 0;
-    }
-
-    /* png is RGBA, but UEFI needs BGRA */
-    if(gop->Mode->Information->PixelFormat == PixelBlueGreenRedReserved8BitPerColor ||
-        (gop->Mode->Information->PixelFormat == PixelBitMask && gop->Mode->Information->PixelInformation.BlueMask != 0xff0000)) {
-        for(l = 0; l < w * h; l++)
-            data[l] = ((data[l] & 0xff) << 16) | (data[l] & 0xff00) | ((data[l] >> 16) & 0xff);
-    }
-    /* display image */
-    gop->Blt(gop, data, EfiBltBufferToVideo, 0, 0, (gop->Mode->Information->HorizontalResolution - w) / 2,
-        (gop->Mode->Information->VerticalResolution - h) / 2, w, h, 0);
-
-    /* free resources exit */
-    free(data);
-    free(buff);
-
     // halt cpu.
     while (1)
         __asm__("hlt");
