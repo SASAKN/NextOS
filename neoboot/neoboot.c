e2c76ba114d628642dec45c03ef6df77b61f5f93
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-23 21:45:25 +0900

    なんか無理

--- neoboot/neoboot.c
+++ neoboot/neoboot.c
@@ -1,315 +1,183 @@
 #include "efilib/inc/efi.h"
 #include "efilib/inc/efilib.h"
 #include "include/elf.h"
-#include "include/memmap.h"
-#include "include/param.h"
 
 #define MAX(a, b) (a < b ? b : a)
 #define MIN(a, b) (a < b ? a : b)
 
-void hlt(void) {
-    while(1) __asm__ ("hlt");
+#define align_up(x, align)	\
+	((__typeof__(x))(((uintptr_t)(x)+((align)-1))&(~((align)-1))))
+
+static void print_efi_memory_descriptor(EFI_MEMORY_DESCRIPTOR *d) {
+    Print(
+        L"%x %x %x %x\n",
+        d->Type,
+        d->PhysicalStart,
+        d->NumberOfPages,
+        d->Attribute
+    );
 }
 
-void print_efi_configuration_table(void)
-{
-    UINT64 i;
-    Print(L"\n[ INFO ] EfiConfigurationTable\n");
-    for (i = 0; i < ST->NumberOfTableEntries; i++)
-    {
-        Print(L"%02d : %016x : %08x, %04x, %04x\n", i, (unsigned long long)&ST->ConfigurationTable[i], ST->ConfigurationTable[i].VendorGuid.Data1, ST->ConfigurationTable[i].VendorGuid.Data2, ST->ConfigurationTable[i].VendorGuid.Data3);
-        unsigned char j;
-        for (j = 0; j < 8; j++)
-        {
-            Print(L"%02x", ST->ConfigurationTable[i].VendorGuid.Data4[j]);
-        }
-        Print(L"%016x\n", (unsigned long long)ST->ConfigurationTable[i].VendorTable);
-    }
-    Print(L"\n");
-    PrintOK();
-    Print(L"VendorTable\n");
-};
-
-CHAR16 *get_memtype_name(EFI_MEMORY_TYPE type)
-{
-    switch (type)
-    {
-    case EfiReservedMemoryType:
-        return L"EfiReservedMemoryType";
-    case EfiLoaderCode:
-        return L"EfiLoaderCode";
-    case EfiLoaderData:
-        return L"EfiLoaderData";
-    case EfiBootServicesCode:
-        return L"EfiBootServicesCode";
-    case EfiBootServicesData:
-        return L"EfiBootServicesData";
-    case EfiRuntimeServicesCode:
-        return L"EfiRuntimeServicesCode";
-    case EfiRuntimeServicesData:
-        return L"EfiRuntimeServicesData";
-    case EfiConventionalMemory:
-        return L"EfiConventionalMemory";
-    case EfiUnusableMemory:
-        return L"EfiUnusableMemory";
-    case EfiACPIReclaimMemory:
-        return L"EfiACPIReclaimMemory";
-    case EfiACPIMemoryNVS:
-        return L"EfiACPIMemoryNVS";
-    case EfiMemoryMappedIO:
-        return L"EfiMemoryMappedIO";
-    case EfiMemoryMappedIOPortSpace:
-        return L"EfiMemoryMappedIOPortSpace";
-    case EfiPalCode:
-        return L"EfiPalCode";
-    case EfiPersistentMemory:
-        return L"EfiPersistentMemory";
-    case EfiMaxMemoryType:
-        return L"EfiMaxMemoryType";
-    default:
-        return L"InvalidMemoryType";
-    }
-};
-
-void PrintOK(void) {
-    ST->ConOut->SetAttribute(ST->ConOut, EFI_GREEN);
-    Print(L"[ OK ]");
-    ST->ConOut->SetAttribute(ST->ConOut, EFI_WHITE);
+// I don't know why, but gnu-efi's CopyMem didn't copy anything, so I use this memcpy
+static void *memcpy(void *dst, const void *src, size_t n) {
+    uint8_t *d = (uint8_t *) dst;
+    const uint8_t *s = (const uint8_t *) src;
+    while (n--)
+        *d++ = *s++;
+    return dst;
 }
 
-void PrintError(void) {
-    ST->ConOut->SetAttribute(ST->ConOut, EFI_RED);
-    Print(L"[ Error ]");
-    ST->ConOut->SetAttribute(ST->ConOut, EFI_WHITE);
-}
-
-void PrintWarn(void) {
-    ST->ConOut->SetAttribute(ST->ConOut, EFI_YELLOW);
-    Print(L"[ Warn ]");
-    ST->ConOut->SetAttribute(ST->ConOut, EFI_WHITE);
-}
-
-void PrintGoodBye(void) {
-    ST->ConOut->SetAttribute(ST->ConOut, EFI_BLUE);
-    Print(L"[ GoodBye ]");
-    ST->ConOut->SetAttribute(ST->ConOut, EFI_WHITE);
-}
-
-//EDK2から移植
-UINTN EFIAPI AsciiSPrint( OUT CHAR8 *StartOfBuffer,IN  UINTN BufferSize,IN CONST CHAR8  *FormatString, ...){
-  va_list  Marker;
-  UINTN    size;
-  va_start (Marker, FormatString);
-  size = AsciiVSPrint(StartOfBuffer, BufferSize, FormatString, Marker);
-  va_end (Marker);
-  return size;
-}
-
-UINTN EFIAPI AsciiStrLen ( IN CONST CHAR8 *String )
-{
-  UINTN  Length;
-  ASSERT (String != NULL);
-  while (String[Length] != '\0') {
-    Length++;
-  }
-  return Length;
-}
-
-//@End EDK2から移植
-
-EFI_STATUS print_memmap(struct MemoryMap *map) {
-    EFI_STATUS status;
-    Print(L"\n[ INFO ] MemoryMap\n");
-    CHAR16 *header = L"Index, Buffer, Type, Type(name),PhysicalStart, VirtualStart, NumberOfPages, Size,  Attribute";
-    Print(L"%s\n", header);
-    uint32_t i;
-    EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)map->buffer;
-    for (i = 0; i < map->memmap_desc_entry; i++) {
-        Print(L"%02d, %016x, %02x, %s, %016x, %016x, %016x, %d, %016x",  i, desc, desc->Type, get_memtype_name(desc->Type), desc->PhysicalStart, desc->VirtualStart, desc->NumberOfPages, desc->NumberOfPages, desc->Attribute);
-        desc = (EFI_MEMORY_DESCRIPTOR *)((uint8_t *)desc + map->descriptor_size);
-    }
-    Print(L"\n");
-    PrintOK();
-    Print(L"Print Memory Map\n");
-    return EFI_SUCCESS;
-}
-
-// Mikanosのブートローダーから移植
-
-EFI_STATUS open_root_dir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL** root) {
-  EFI_LOADED_IMAGE_PROTOCOL* loaded_image;
-  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fs;
-  EFI_STATUS status = 0;
-  status = uefi_call_wrapper(BS->OpenProtocol, 6, image_handle, &gEfiLoadedImageProtocolGuid, (VOID**)&loaded_image, image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
-  ASSERT(status != EFI_SUCCESS);
-  status = uefi_call_wrapper(BS->OpenProtocol, 6, loaded_image->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (VOID**)&fs, image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
-  ASSERT(status != EFI_SUCCESS);
-  status = uefi_call_wrapper(fs->OpenVolume, 2, fs, root);
-  ASSERT(status != EFI_SUCCESS);
-  PrintOK();
-  Print(L"Open Root Directory\n");
-  return status;
-}
-
-// Calc and Load Address range.
-void calc_load_address_range(elf64_ehdr *ehdr, UINT64 *first, UINT64 *last) {
-    elf64_phdr *phdr = (elf64_phdr *)((UINT64)ehdr + ehdr->e_phoff);
-    *first = UINT64_MAX;
-    *last = 0;
-    for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i) {
-        if (phdr[i].p_type != PT_LOAD) continue;
-        *first = MIN(*first, phdr[i].p_paddr);
-        *last = MAX(*last, phdr[i].p_vaddr + phdr[i].p_memsz);
-    }
-}
-
-
-// Copy and Load segments.
-void copy_load_segments(elf64_ehdr *ehdr) {
-    elf64_phdr *phdr = (elf64_phdr *)((UINT64)ehdr + ehdr->e_phoff);
-    for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i) {
-        if (phdr[i].p_type != PT_LOAD) continue;
-        UINT64 segment_in_file = (UINT64)ehdr + phdr[i].p_offset;
-        CopyMem((VOID**)phdr[i].p_vaddr, (VOID*)segment_in_file, phdr[i].p_filesz);
-        UINTN remain_bytes = phdr[i].p_memsz - phdr[i].p_filesz;
-        SetMem((VOID*)(phdr[i].p_vaddr + phdr[i].p_filesz), remain_bytes, 0);
-    }
-}
-
-// @End Mikanosのブートローダーから移植
-
-EFI_STATUS save_memmap(struct MemoryMap *map, EFI_FILE_PROTOCOL *file) {
-    EFI_STATUS status;
-    CHAR8 buf[300];
-    UINTN size;
-    CHAR8 *header = "Index, Buffer, Type, Type(name),PhysicalStart, VirtualStart, NumberOfPages, Size,  Attribute";
-    size = AsciiStrLen(header);
-    status = uefi_call_wrapper(file->Write, 3, file, &size, header);
-    ASSERT(status != EFI_SUCCESS);
-    EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)map->buffer;
-    for (uint32_t i = 0; i < map->memmap_desc_entry; i++){
-        size = AsciiSPrint(buf, sizeof(buf), "%02d, %016x, %02x, %s, %016x, %016x, %016x, %d, %016x \n", i, desc, desc->Type, get_memtype_name(desc->Type), desc->PhysicalStart, desc->VirtualStart, desc->NumberOfPages, desc->NumberOfPages, desc->Attribute);
-        status = uefi_call_wrapper(file->Write, 3, file, &size, buf);
-        ASSERT(status != EFI_SUCCESS);
-        desc = (EFI_MEMORY_DESCRIPTOR *)((uint8_t *)desc + map->descriptor_size);
-    }
-    PrintOK();
-    Print(L"Save Memory Map\n");
-    return EFI_SUCCESS;
-}
-
-EFI_STATUS load_kernel(EFI_FILE_PROTOCOL *root_dir, EFI_FILE_PROTOCOL *kernel_file, UINT64 kernel_first_addr, UINT64 kernel_last_addr) {
-    //Load File
-    EFI_STATUS status = 0;
-    CHAR16 *file_name = L"\\kernel.elf";
-    status = uefi_call_wrapper(root_dir->Open, 5, root_dir, &kernel_file, file_name, EFI_FILE_MODE_READ, 0);
-    if (EFI_ERROR(status)) {
-        PrintError();
-        Print(L"Open file '\\kernel.elf' : %r\n", status);
-    }
-    // Get File Info
-    UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * (StrLen(file_name));
-    UINT8 file_info_buffer[file_info_size];
-    status = uefi_call_wrapper(kernel_file->GetInfo, 4, kernel_file, &gEfiFileInfoGuid, &file_info_size, file_info_buffer);
-    if (EFI_ERROR(status)) {
-        PrintError();
-        Print(L"Get file info '\\kernel.elf' : %r\n", status);
-    }
-    EFI_FILE_INFO *file_info = (EFI_FILE_INFO *)file_info_buffer;
-    UINTN kernel_file_size = file_info->FileSize;
-    Print(L"[ INFO ] Kernel File Size : %ld", kernel_file_size);
-    // Allocate (tmp)
-    VOID *kernel_buffer;
-    status = uefi_call_wrapper(BS->AllocatePool, 3, EfiLoaderData, kernel_file_size, &kernel_buffer);
-    if (EFI_ERROR(status)) {
-        PrintError();
-        Print(L"allocate pool : %r\n", status);
-    }
-    status = uefi_call_wrapper(kernel_file->Read, 3, kernel_file, &kernel_file_size, kernel_buffer);
-    if (EFI_ERROR(status)) {
-        PrintError();
-        Print(L"read kernel file : %r\n", status);
-    }
-    // Allocate Pages
-    elf64_ehdr* kernel_ehdr;
-    kernel_ehdr = (elf64_ehdr *)kernel_buffer;
-    calc_load_address_range(kernel_ehdr, &kernel_first_addr, &kernel_last_addr);
-    UINTN num_pages = (kernel_last_addr - kernel_first_addr + 0xfff) / 0x1000;  //4kibのUEFIが読み取れるメモリーのブロックを作成
-    status = uefi_call_wrapper(BS->AllocatePages, 4, AllocateAddress, EfiLoaderData, num_pages, &kernel_first_addr);
-    if (EFI_ERROR(status)) {
-        PrintError();
-        Print(L"Allocate Pages : %r", status);
-    }
-    // Copy Segments
-    copy_load_segments(kernel_ehdr);
-    Print(L"Kernel: 0x%0lx - 0x%0lx\n", kernel_first_addr, kernel_last_addr);
-    status = uefi_call_wrapper(BS->FreePool, 1, kernel_buffer);
-    if (EFI_ERROR(status)) {
-        PrintError();
-        Print(L"Free Pool");
-    }
-    return status;
-}
-
-EFI_STATUS exit_bs(struct MemoryMap *map, EFI_HANDLE ImageHandle){
-    EFI_STATUS status;
-    status = uefi_call_wrapper(BS->ExitBootServices, 2, ImageHandle, map->map_key);
-    if (EFI_ERROR(status)) {
-        EFI_MEMORY_DESCRIPTOR *m_map = LibMemoryMap(&map->memmap_desc_entry, &map->map_key, &map->descriptor_size, &map->descriptor_version);
-        if (m_map == NULL) {
-            PrintError();
-            Print(L"Get Memory Map Key : %r\n", status);
-        }
-        status = uefi_call_wrapper(BS->ExitBootServices, 2, ImageHandle, map->map_key);
-        if (EFI_ERROR(status)) {
-            PrintError();
-            Print(L"Exit Boot Services : %r\n", status);
-        }
-    }
-    return status;
-}
-
-EFI_STATUS EFIAPI efimain (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    EFI_STATUS status = 0;
+EFI_STATUS
+EFIAPI
+efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     InitializeLib(ImageHandle, SystemTable);
-    Print(L"OK!");
-    uefi_call_wrapper(ST->BootServices->SetWatchdogTimer, 4, 0, 0, 0, NULL);
-    //Welcome
-    PrintOK();
-    Print(L"Welcome to NeoBoot v.0.1");
-    //Get MemoryMap
-    struct MemoryMap map;
-    EFI_MEMORY_DESCRIPTOR *efi_map = LibMemoryMap(&map.memmap_desc_entry, &map.map_key, &map.descriptor_size, &map.descriptor_version);
-    map.memmap_desc_entry = map.map_size / map.descriptor_size;
+
+    // clear screen
+    SIMPLE_TEXT_OUTPUT_INTERFACE *conout = ST->ConOut;
+    uefi_call_wrapper(conout->ClearScreen, 1, conout);
+
+    Print(L"Booting...\n");
+
+    // print memory map
+    UINTN num_entry, map_key, desc_size;
+    UINT32 desc_version;
+
+    EFI_MEMORY_DESCRIPTOR *map = LibMemoryMap(
+        &num_entry,
+        &map_key,
+        &desc_size,
+        &desc_version
+    );
     ASSERT(map != NULL);
-    PrintOK();
-    Print(L"Get Memory Map");
-    // Print MemoryMap
-    print_memmap(&map);
-    //OpenRootDir
-    EFI_FILE_PROTOCOL *root_dir;
-    open_root_dir(ImageHandle, &root_dir);
-    // Save Memory Map
-    EFI_FILE_PROTOCOL *memmap_file;
-    uefi_call_wrapper(root_dir->Open, 5, root_dir, &memmap_file, L"\\memmap", EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE);
-    save_memmap(&map, memmap_file);
-    // Init GOP
-    // Prepare Kernel Paramater
-    // Load Kernel
-    EFI_FILE_PROTOCOL *kernel_file;
-    UINT64 kernel_first_addr , kernel_last_addr;
-    load_kernel(root_dir, kernel_file, kernel_first_addr, kernel_last_addr);
-    // GoodBye
-    PrintGoodBye();
-    Print(L"BootLoader\n");
-    // Exit Boot Loader
-    exit_bs(&map, ImageHandle);
-    //注意:これ以降Printなどが呼べなくなります。
-    //つまり開発者は、これ以降に、ジャンプなどの関数以外は、入れては、いけないということです。
-    //入れると一般保護例外や、ページフォルトなどの例外が起きプログラムが正常に動作しなくなります。
-    // 注意してください。
-    UINT64 e_addr = *(UINT64 *)(kernel_first_addr + 24);
-    typedef void e_point_type_t(void);
-    e_point_type_t *entry_point = (e_point_type_t*)e_addr;
-    entry_point();
+
+    Print(L"Type PhysicalStart NumberOfPages Attribute\n");
+    for(UINTN i = 0 ; i < num_entry; i++) {
+        EFI_MEMORY_DESCRIPTOR *d = (EFI_MEMORY_DESCRIPTOR *)((char *)map + desc_size * i);
+        if(d->Type == EfiConventionalMemory)
+            print_efi_memory_descriptor(d);
+    }
+    FreePool(map);
+
+    EFI_STATUS status;
+
+    // open root dir
+    EFI_GUID    lip_guid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
+    EFI_LOADED_IMAGE_PROTOCOL *lip;
+    status = uefi_call_wrapper(
+        BS->OpenProtocol, 6, 
+        ImageHandle, 
+        &lip_guid,
+        (void **) &lip,
+        ImageHandle, 
+        NULL,
+        EFI_OPEN_PROTOCOL_GET_PROTOCOL
+    );
+    ASSERT(!EFI_ERROR(status));
+
+    EFI_FILE_HANDLE root = LibOpenRoot(lip->DeviceHandle);
+    ASSERT(root != NULL);
+
+    // open file
+    EFI_FILE_HANDLE fhandle;
+    status = uefi_call_wrapper(
+        root->Open,
+        5, root, &fhandle, L"kernel.elf", EFI_FILE_MODE_READ, 0
+    );
+    ASSERT(!EFI_ERROR(status));
+
+    // get file size
+    EFI_FILE_INFO *info = LibFileInfo(fhandle);
+    UINT64 fsize = info->FileSize;
+    FreePool(info);
+
+    // allocate pool
+    elf64_ehdr *ehdr = AllocatePool(fsize);
+    ASSERT(ehdr != NULL);
+    
+    // read file
+    status = uefi_call_wrapper(
+        fhandle->Read, 3, 
+        fhandle, 
+        &fsize, 
+        ehdr
+    );
+    ASSERT(!EFI_ERROR(status));
+    //DumpHex(4, 0, fsize, ehdr);
+
+    // calcurate address range
+    UINT64 start = 0, end = 0;
+    for(Elf64_Half i = 0; i < ehdr->e_phnum; i++) {
+        elf64_phdr *phdr = (elf64_phdr *)((char *)ehdr + ehdr->e_phoff + ehdr->e_phentsize * i);
+        if(phdr->p_type != PT_LOAD) continue;
+
+        if(!start) {
+            start = phdr->p_paddr;
+            end = phdr->p_paddr + phdr->p_memsz;
+        }
+
+        start = MIN(start, phdr->p_paddr);
+        end = MAX(end, phdr->p_paddr + phdr->p_memsz);
+    }
+
+    EFI_PHYSICAL_ADDRESS base = 0x100000;
+    
+    // allocate pool
+    UINTN num_pages = align_up(end - start, 0x1000) / 0x1000;
+    Print(L"start = %x, end = %x, num_pages = %x\n", start, end, num_pages);
+
+    status = uefi_call_wrapper(
+        BS->AllocatePages, 4, 
+        AllocateAddress, 
+        EfiLoaderData,
+        num_pages,
+        &base
+    );
+    ASSERT(!EFI_ERROR(status));
+    Print(L"base = %x\n", base);
+
+    // init
+    ZeroMem((VOID *)base, num_pages);
+
+    // copy LOAD segments
+    for(Elf64_Half i = 0; i < ehdr->e_phnum; i++) {
+        elf64_phdr *phdr = (elf64_phdr *)((char *)ehdr + ehdr->e_phoff + ehdr->e_phentsize * i);
+        if(phdr->p_type != PT_LOAD) continue;
+        //DumpHex(4, 0, phdr->p_filesz, (char *)ehdr + phdr->p_offset);
+        memcpy(
+            (VOID *)phdr->p_paddr, 
+            (char *)ehdr + phdr->p_offset, 
+            phdr->p_filesz
+        );
+        //DumpHex(4, 0, phdr->p_memsz, (VOID *)phdr->p_paddr);
+    }
+
+    // get entry point
+    typedef void entry_t (void);
+    entry_t *e_entry = (entry_t *)ehdr->e_entry;
+    Print(L"entry = %x\n", e_entry);
+
+    // free pool
+    FreePool(ehdr);
+
+    // exit boot services
+    LibMemoryMap(
+        &num_entry,
+        &map_key,
+        &desc_size,
+        &desc_version
+    );
+    status = uefi_call_wrapper(
+        BS->ExitBootServices, 2, 
+        ImageHandle,
+        map_key
+    );
+    ASSERT(!EFI_ERROR(status));
+    
+    // jmp to entry
+    __asm__ __volatile__ ("jmp %0\n" :: "r"(e_entry));
+
+    // never reach here
+    while(1);
+
+    return EFI_SUCCESS;
 }
