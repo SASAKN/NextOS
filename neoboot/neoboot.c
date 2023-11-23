afa12ceb40271ce02f8f044721598c4996dfecb2
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-23 17:30:28 +0900

    諸々gnu-efiでできるようにしたり書いたり

--- neoboot/neoboot.c
+++ neoboot/neoboot.c
@@ -1,3 +1,4 @@
+#define EFI_DEBUG
 #include "efilib/inc/efi.h"
 #include "efilib/inc/efilib.h"
 #include "include/elf.h"
@@ -6,6 +7,10 @@
 
 #define MAX(a, b) (a < b ? b : a)
 #define MIN(a, b) (a < b ? a : b)
+
+void hlt(void) {
+    while(1) __asm__ ("hlt");
+}
 
 void print_efi_configuration_table(void)
 {
@@ -135,29 +140,46 @@ EFI_STATUS print_memmap(struct MemoryMap *map) {
 EFI_STATUS open_root_dir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL** root) {
   EFI_LOADED_IMAGE_PROTOCOL* loaded_image;
   EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fs;
-  uefi_call_wrapper(BS->OpenProtocol, 6, image_handle, &gEfiLoadedImageProtocolGuid, (VOID**)&loaded_image, image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
-  uefi_call_wrapper(BS->OpenProtocol, 6, loaded_image->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (VOID**)&fs, image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
-  uefi_call_wrapper(fs->OpenVolume, 2, fs, root);
+  status = uefi_call_wrapper(BS->OpenProtocol, 6, image_handle, &gEfiLoadedImageProtocolGuid, (VOID**)&loaded_image, image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+  ASSERT(status != EFI_SUCCESS);
+  status = uefi_call_wrapper(BS->OpenProtocol, 6, loaded_image->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (VOID**)&fs, image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+  ASSERT(status != EFI_SUCCESS);
+  status = uefi_call_wrapper(fs->OpenVolume, 2, fs, root);
+  ASSERT(status != EFI_SUCCESS);
+  PrintOK();
+  Print(L"Open Root Directory\n");
   return EFI_SUCCESS;
 }
 
 // @End Mikanosのブートローダーから移植
 
 EFI_STATUS save_memmap(struct MemoryMap *map, EFI_FILE_PROTOCOL *file) {
+    EFI_STATUS status;
     CHAR8 buf[300];
     UINTN size;
     CHAR8 *header = "Index, Buffer, Type, Type(name),PhysicalStart, VirtualStart, NumberOfPages, Size,  Attribute";
     size = AsciiStrLen(header);
-    uefi_call_wrapper(file->Write, 3, file, &size, header);
+    status = uefi_call_wrapper(file->Write, 3, file, &size, header);
+    ASSERT(status != EFI_SUCCESS);
     EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)map->buffer;
     for (uint32_t i = 0; i < map->memmap_desc_entry; i++){
         size = AsciiSPrint(buf, sizeof(buf), "%02d, %016x, %02x, %s, %016x, %016x, %016x, %d, %016x \n", i, desc, desc->Type, get_memtype_name(desc->Type), desc->PhysicalStart, desc->VirtualStart, desc->NumberOfPages, desc->NumberOfPages, desc->Attribute);
-        uefi_call_wrapper(file->Write, 3, file, &size, buf);
+        status = uefi_call_wrapper(file->Write, 3, file, &size, buf);
+        ASSERT(status != EFI_SUCCESS);
         desc = (EFI_MEMORY_DESCRIPTOR *)((uint8_t *)desc + map->descriptor_size);
     }
     PrintOK();
     Print(L"Save Memory Map\n");
     return EFI_SUCCESS;
+}
+
+EFI_STATUS load_kernel(EFI_FILE_PROTOCOL *root_dir, EFI_FILE_PROTOCOL *kernel_file) {
+    status = uefi_call_wrapper(root_dir->Open, 5, root_dir, &kernel_file, L"\\kernel.elf", EFI_FILE_MODE_READ, 0);
+    if (EFI_ERROR(status)) {
+        PrintError();
+        Print(L"Open file '\\kernel.elf' : %r\n", status);
+        hlt();
+    }
 }
 
 EFI_STATUS EFIAPI main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
@@ -182,6 +204,11 @@ EFI_STATUS EFIAPI main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     EFI_FILE_PROTOCOL *memmap_file;
     uefi_call_wrapper(root_dir->Open, 5, root_dir, &memmap_file, L"\\memmap", EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE);
     save_memmap(&map, memmap_file);
+    // Init GOP
+    // Prepare Kernel Paramater
+    // Load Kernel
+    EFI_FILE_PROTOCOL *kernel_file;
+    load_kernel(root_dir, kernel_file);
     // GoodBye
     PrintGoodBye();
     Print(L"BootLoader");
