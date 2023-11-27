3f21dc17ebd72832b7704bbf449935b494b8c343
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-27 22:06:10 +0900

    必要なファイルを整える

---
+++ neos_bootloader/include/mem.h
@@ -0,0 +1,27 @@
+#ifndef _BOOT_MEM_H
+#define _BOOT_MEM_H
+
+#define MEM_DESC_SIZE	4096
+
+/* A memory map for booting */
+struct MemoryMap {
+    UINT64 buffer_size;
+    EFI_MEMORY_DESCRIPTOR* buffer;
+    UINT64 map_size;
+    UINT64 map_key;
+    UINT64 descriptor_size;
+    UINT32 descriptor_version;
+    UINT64 memmap_desc_entry;
+};
+
+/* EFI_MEMORY_DESCRIPTOR */
+struct MemoryDescriptor {
+    UINT64 Type;
+    UINT32 PhysicalStart;
+    UINT32 VirtualStart;
+    UINT32 NumberOfPages;
+    UINT32 Attribute;
+};
+
+
+#endif
---
+++ neos_bootloader/make_image.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Neos Boot Loader Build Tool (UEFI Development)
+
+script_dir="$(dirname "$(readlink -f "$0")")"
+
+#ルートファイルシステムを作成
+source ${script_dir}/make_root_fs.sh
+
+#DMGファイルの作成
+hdiutil create -size 1g -fs MS-DOS -volname NEOS neos.img
+
+#DMGファイルの名前 neos.img.dmg
+
+#DMGファイルをCDRファイルに変換
+hdiutil convert neos.img.dmg -format UDTO -o neos.img
+
+#CDRファイルの名前 neos.img.cdr
+
+#CDRファイルをIMGファイルに変換
+mv neos.img.cdr neos.img
+
+#マウント
+hdiutil mount neos.img
+
+#それに移動
+cd /Volumes/NEOS
+
+#ファイル構成の作成
+mkdir -p EFI/BOOT
+
+#さらに、それの階層に移動
+cd /Volumes/NEOS/EFI/BOOT
+
+#移動したら、ファイルを追加
+cp ${script_dir}/${1} ./BOOTX64.efi
+
+#それをコピー
+cp -r ${script_dir}/neos/ /Volumes/NEOS/neos/
+
+#ルートフォルダーよりもカーネル
+cp ${script_dir}/kernel.elf /Volumes/NEOS/kernel.elf
+
+#作業フォルダーに戻る
+cd ${script_dir}
+
+#アンマウント
+hdiutil unmount /Volumes/NEOS -force
+
+#QEMUの実行に移る
+command ${script_dir}/run_image.sh
