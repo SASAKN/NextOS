01d209bb80d8131561fec942a25f4906d94f94ef
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-29 22:46:49 +0900

    プロトタイプ宣言を書いた

---
+++ neoboot/common.h
@@ -0,0 +1,10 @@
+#ifndef _NEOBOOT_COMMON_H
+#define _NEOBOOT_COMMON_H
+
+// Prototype
+void PrintError(void);
+void PrintOK(void);
+void PrintGoodBye(void);
+void PrintWarn(void);
+
+#endif // _NEOBOOT_COMMON_H
---
+++ neoboot/neoboot.c
@@ -0,0 +1,17 @@
+// For using UEFI Library
+#include  <Uefi.h>
+#include  <Library/UefiLib.h>
+#include  <Library/UefiBootServicesTableLib.h>
+#include  <Library/PrintLib.h>
+#include  <Library/MemoryAllocationLib.h>
+#include  <Library/BaseMemoryLib.h>
+#include  <Protocol/LoadedImage.h>
+#include  <Protocol/SimpleFileSystem.h>
+#include  <Guid/FileInfo.h>
+
+// Base Include Files
+#include "mem.h"
+#include "file.h"
+#include "elf.h"
+
+
