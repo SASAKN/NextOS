a802553891d057ac246f2cba4b3a4cc5de489e0e
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-29 22:42:00 +0900

    これで、OK

--- neoboot/file.h
+++ neoboot/file.h
@@ -2,5 +2,7 @@
 #define _NEOBOOT_FILE_H
 
 // ProtoType
+EFI_STATUS open_root_dir(EFI_HANDLE IM, EFI_FILE_PROTOCOL **root);
+EFI_FILE_PROTOCOL *create_file(EFI_FILE_PROTOCOL *root, CHAR16 *file_path);
 
 #endif //_NEOBOOT_FILE_H
