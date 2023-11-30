afcdfe2de449ee24c53007309ff4c12cac41bd99
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-11-30 22:26:11 +0900

    neobootの削除

--- neoboot/file.h
+++
@@ -1,8 +0,0 @@
-#ifndef _NEOBOOT_FILE_H
-#define _NEOBOOT_FILE_H
-
-// ProtoType
-EFI_STATUS open_root_dir(EFI_HANDLE IM, EFI_FILE_PROTOCOL **root);
-EFI_FILE_PROTOCOL *create_file(EFI_FILE_PROTOCOL *root, CHAR16 *file_path);
-
-#endif //_NEOBOOT_FILE_H
--- neoboot/move_to_mac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-# Move to Mac
-
-script_dir="$(dirname "$(readlink -f "$0")")"
-
-cp /Users/kenta/edk2/Build/NeoBootX64/DEBUG_CLANGPDB/X64/loader.efi ${script_dir}/loader.efi
