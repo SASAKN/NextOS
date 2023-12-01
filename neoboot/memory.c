5f1aa23898e4cd6516ffcd23ad6aecaa705b3732
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-12-01 22:54:39 +0900

    Getするだけじゃない

--- neoboot/memory.c
+++ neoboot/memory.c
@@ -2,6 +2,9 @@
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
 #include <Library/PrintLib.h>
+#include  <Protocol/LoadedImage.h>
+#include  <Protocol/SimpleFileSystem.h>
+#include  <Guid/FileInfo.h>
 #include "efiio.h"
 
 const CHAR16 *get_memtype(EFI_MEMORY_TYPE type) {
@@ -69,4 +72,53 @@ EFI_STATUS get_memmap(memmap *map) {
         }
         return EFI_SUCCESS;
     }
+}
+
+EFI_STATUS print_memmap(memmap *map) {
+    Print(L"\n [ INFO ] Memory Map \n");
+    UINT16 *header = L"Index, Buffer, Type, Type(name), PhysicalStart, VirtualStart, NumberOfPages, Size, Attribute";
+    Print(L"%-ls\n", header);
+    EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)map->buffer;
+    for (UINT32 i = 0; i < map->memmap_desc_entry; i++) {
+        Print(L"%02d, %016x, %02x, %-ls, %016x, %016x, %016x, %d, %016x \n", i, desc, desc->Type, get_memtype(desc->Type), desc->PhysicalStart, desc->VirtualStart, desc->NumberOfPages, desc->NumberOfPages, desc->Attribute);
+        desc = (EFI_MEMORY_DESCRIPTOR *)((UINT8 *)desc + map->descriptor_size);
+    }
+    Print(L"\n");
+    PrintOK();
+    Print(L"Print Memory Map\n");
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS save_memmap(memmap *map, EFI_FILE_PROTOCOL *f, EFI_FILE_PROTOCOL *root_dir) {
+    char buffer[512];
+    EFI_STATUS status;
+    UINTN size;
+    // Header
+    CHAR8 *header = "Index, Buffer, Type, Type(name), PhysicalStart, VirtualStart, NumberOfPages, Size, Attribute";
+    size = AsciiStrLen(header);
+    // Open memory map file
+    status = root_dir->Open(root_dir, &f, L"\\memmap.blmm", EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
+    if (EFI_ERROR(status)) {
+        PrintError();
+        Print(L"Create a file : %r\n", status);
+    }
+    // Write header
+    status = f->Write(f, &size, header);
+    if (EFI_ERROR(status)) {
+        PrintError();
+        Print(L"Write header : %r\n", status);
+    }
+    // Write memory map
+    EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)map->buffer;
+    for (UINT32 i = 0; i < map->memmap_desc_entry; i++) {
+        size = AsciiSPrint(buffer, sizeof(buffer), "%02u, %016x, %02x, %-ls, %016x, %016x, %016x, %d, %016x \n", i, desc, desc->Type, get_memtype(desc->Type), desc->PhysicalStart, desc->VirtualStart, desc->NumberOfPages, desc->NumberOfPages, desc->Attribute);
+        status = f->Write(f, &size, header);
+        if (EFI_ERROR(status)) {
+            PrintError();
+            Print(L"Write Memory Map : %r\n", status);
+        }
+        desc = (EFI_MEMORY_DESCRIPTOR *)((UINT8 *)desc + map->descriptor_size);
+    }
+    f->Close(f);
+    return EFI_SUCCESS;
 }
