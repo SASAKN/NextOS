d123e26b2d40558d29884533ba61d21af3817eff
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-12-01 22:59:46 +0900

    includeを継ぎ足し

--- neoboot/memory.h
+++ neoboot/memory.h
@@ -2,6 +2,7 @@
 #define _MEM_H
 
 #include <Uefi/UefiBaseType.h>
+#include <Protocol/SimpleFileSystem.h>
 
 #define INIT_MAP_SIZE 128
 
@@ -14,6 +15,13 @@ typedef struct memory_map {
     UINTN desc_size;
     UINTN desc_ver;
 } memmap;
+
+// Prototype
+const CHAR16 *get_memtype(EFI_MEMORY_TYPE type);
+EFI_STATUS allocate_memmap(memmap *map, UINTN buffer_size);
+EFI_STATUS get_memmap(memmap *map);
+EFI_STATUS print_memmap(memmap *map);
+EFI_STATUS save_memmap(memmap *map, EFI_FILE_PROTOCOL *f, EFI_FILE_PROTOCOL *root_dir);
 
 
 #endif // _MEM_H
