6f2f311099408363f618222880e98412c9966499
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-12-01 23:15:16 +0900

    今日は、ここまで

--- neoboot/efiio.c
+++ neoboot/efiio.c
@@ -1,9 +1,11 @@
 #include <Uefi/UefiBaseType.h>
-#include <Uefi/UefiSpec.h>
-#include <Library/PrintLib.h>
+#include <Protocol/SimpleFileSystem.h>
 #include <Library/UefiBootServicesTableLib.h>
+#include <Protocol/LoadedImage.h>
+#include <Guid/FileInfo.h>
+#include <Library/BaseLib.h>
+#include <Library/UefiLib.h>
 
-/* OKを緑で表示 */
 void PrintOK(void)
 {
     gST->ConOut->SetAttribute(gST->ConOut, 0x02); /* 緑で、OKを表示 */
@@ -24,3 +26,44 @@ void PrintError(void)
     gST->ConOut->OutputString(gST->ConOut, L"[ Error ! ]");
     gST->ConOut->SetAttribute(gST->ConOut, 0x0F); /* 白に戻す */
 };
+
+// Open root directory
+EFI_STATUS open_root_dir(EFI_HANDLE IM, DIR **root) {
+    EFI_STATUS status;
+    EFI_LOADED_IMAGE_PROTOCOL *lip;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fs;
+    EFI_HANDLE fs_opener;
+    // Loaded Image Protocol
+    status = gBS->OpenProtocol(IM, &gEfiLoadedImageProtocolGuid, (VOID**)&lip, IM, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+    if (EFI_ERROR(status)) {
+        PrintError();
+        Print(L"Open Protocol : %r\n", status);
+    }
+    // Simple File System Protocol
+    status = gBS->OpenProtocol(lip->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (VOID**)&fs, IM, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+    if (EFI_ERROR(status)) {
+        PrintError();
+        Print(L"Open Protocol : %r\n", status);
+    }
+    // Open Volume
+    status = fs->OpenVolume(fs, root);
+    if (EFI_ERROR(status)) {
+        PrintError();
+        Print(L"Open Protocol : %r\n", status);
+    }
+    // Close Loaded Image Protocol
+    status = gBS->CloseProtocol(IM, &gEfiLoadedImageProtocolGuid, IM, NULL);
+    if (EFI_ERROR(status)) {
+        PrintError();
+        Print(L"Close Loaded Image Protocol : %r\n", status);
+    }
+    // Close Simple File System Protocol
+    status = gBS->CloseProtocol(lip->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, IM, NULL);
+    if (EFI_ERROR(status)) {
+        PrintError();
+        Print(L"Close Simple File System Protocol : %r\n", status);
+    }
+    PrintOK();
+    Print(L"Open Volume\n");
+    return EFI_SUCCESS;
+}
---
+++ neoboot/main.c
@@ -0,0 +1,40 @@
+#include  <Uefi.h>
+#include  <Library/UefiLib.h>
+#include  <Library/UefiBootServicesTableLib.h>
+#include  <Library/UefiRuntimeServicesTableLib.h>
+#include  <Library/PrintLib.h>
+#include  <Library/MemoryAllocationLib.h>
+#include  <Library/BaseMemoryLib.h>
+#include  <Protocol/LoadedImage.h>
+#include  <Protocol/SimpleFileSystem.h>
+#include  <Guid/FileInfo.h>
+
+#include "efiio.h"
+#include "memory.h"
+#include "elf.h"
+
+void init_uefi(void) {
+    gBS->SetWatchdogTimer(0, 0 ,0, NULL);
+    PrintOK();
+    Print(L"Welcome to NextOS \n");
+}
+
+EFI_STATUS EFIAPI main(EFI_HANDLE IM, EFI_SYSTEM_TABLE *sys_table) {
+    // Welcome
+    init_uefi();
+    // Init MemoryMap
+    struct MemoryMap map;
+    char *buffer = NULL;
+    init_memmap(&map, buffer);
+    // Open Root Directory
+    EFI_FILE_PROTOCOL *root;
+    open_root_dir(IM, &root);
+    // Get it, Print it and Save it
+    get_memmap(&map);
+    print_memmap(&map);
+    save_memmap(&map, root);
+    return EFI_SUCCESS;
+} 
+
+
+
