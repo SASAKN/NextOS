b6bfe93f3362203998c3ca492212c8ba3bbb8ad9
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-12-03 21:41:44 +0900

    ELFローダーを作成

--- neoboot/elf_loader.c
+++ neoboot/elf_loader.c
@@ -13,6 +13,8 @@
 #include "elf.h"
 #include "elf_loader.h"
 
+// ELFローダーなし
+#ifndef _NEOBOOT_ELF_LOADER
 EFI_STATUS load_kernel(EFI_FILE_PROTOCOL *root, EFI_FILE_PROTOCOL *kernel_file, UINTN kernel_file_size, EFI_PHYSICAL_ADDRESS kernel_base_addr) {
     // EFI_STATUS status;
     // Open file
@@ -28,10 +30,91 @@ EFI_STATUS load_kernel(EFI_FILE_PROTOCOL *root, EFI_FILE_PROTOCOL *kernel_file, 
     kernel_file_size = file_info->FileSize;
 
     // Allocate kernel
-    gBS->AllocatePages(
-      AllocateAddress, EfiLoaderData,
-      (kernel_file_size + 4095) / 4096, &kernel_base_addr);
+    gBS->AllocatePages(AllocateAddress, EfiLoaderData, (kernel_file_size + 4095) / 4096, &kernel_base_addr);
+    // Read kernel
     kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
 
     return EFI_SUCCESS;
+}
+#endif
+
+// ELFローダーあり
+EFI_STATUS load_kernel(EFI_FILE_PROTOCOL *root, EFI_FILE_PROTOCOL *kernel_file, UINTN kernel_file_size, EFI_PHYSICAL_ADDRESS kernel_base_addr, Elf64_Addr *entry_addr) {
+  EFI_STATUS status;
+  // Open file
+  status = root->Open(root, &kernel_file, L"\\kernel.elf", EFI_FILE_MODE_READ, 0);
+  if(EFI_ERROR(status)) {
+    PrintError();
+    Print(L"Open the kernel file 'kernel.elf'\n");
+  }
+
+  // Get info
+  UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
+  UINT8 file_info_buffer[file_info_size];
+  status =  kernel_file->GetInfo(kernel_file, &gEfiFileInfoGuid, &file_info_size, file_info_buffer);
+  if(EFI_ERROR(status)) {
+    PrintError();
+    Print(L"Get info the kernel file 'kernel.elf'\n");
+  }
+
+  // Get file size
+  EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
+  kernel_file_size = file_info->FileSize;
+  
+  // Allocate pool
+  VOID *kernel_buffer;
+  status = gBS->AllocatePool(EfiLoaderData, kernel_file_size, &file_buffer);
+  if (EFI_ERROR(status)) {
+    PrintError();
+    Print(L"Allocate Pool \n");
+  }
+  // Read the kernel file
+  status = kernel_file->Read(kernel_file, &kernel_file_size, file_buffer);
+  if (EFI_ERROR(status)) {
+    PrintError();
+    Print(L"Read the kernel file \n");
+  } 
+
+  // Read program headers
+  elf64_ehdr *kernel_ehdr = (elf64_ehdr *)kernel_buffer;
+  UINT64 kernel_start_addr, kernel_end_addr;
+  kernel_start_addr = MAX_UINT64;
+  kernel_end_addr = 0;
+
+  // Is the kernel file is executable ?
+  if (kernel_ehdr->e_ident[0] == 0x7f && kernel_ehdr->e_ident[1] == 'E' && kernel_ehdr->e_ident[2] == 'L' && kernel_ehdr->e_ident[3] == 'F' && kernel_ehdr->e_ident[4] == 2 && kernel_ehdr->e_ident[5] == 1 && kernel_ehdr->e_type == ET_EXEC) {
+    continue;
+  } else {
+    PrintError();
+    Print(L"The kernel file is not executable file\n");
+  }
+
+  // Calculate address range
+  elf64_phdr *kernel_phdr = (elf64_phdr*)((UINT64)ehdr + ehdr->e_phoff);
+  for (Elf64_Half i = 0; i < kernel_ehdr->e_phnum; i++) {
+    if (kernel_phdr[i].p_type != PT_LOAD) continue;
+    kernel_start_addr = MIN(kernel_start_addr, kernel_phdr[i].p_vaddr);
+    kernel_end_addr = MAX(kernel_end_addr, phdr[i].p_vaddr + phdr[i].p_memsz);
+  }
+
+  // Allocate pages
+  UINTN num_pages = (kernel_end_addr - kernel_start_addr + 4095) / 4096;
+  status = gBS->AllocatePages(AllocateAddress, EfiLoaderData, num_pages, &kernel_start_addr);
+  if (EFI_ERROR(status)) {
+    PrintError();
+    Print(L"Allocate pages : %r \n", status);
+  }
+
+  // Copy LOAD segments
+  for (Elf64_Half i = 0; i < kernel_ehdr->e_phnum; i++) {
+    if (kernel_phdr[i].p_type != PT_LOAD) continue;
+    UINT64 segment_in_file = (UINT64)kernel_ehdr + kernel_phdr[i].p_offset;
+    CopyMem((VOID *)kernel_phdr[i].p_vaddr, (VOID *)segment_in_file, kernel_phdr[i].p_filesz);
+    UINTN remain_bytes = kernel_phdr[i].p_memsz - kernel_phdr[i].p_filesz;
+    SetMem((VOID *)(kernel_phdr[i].p_vaddr + kernel_phdr[i].p_filesz), remain_bytes, 0);
+  }
+
+  // Locate entry point
+  *entry_addr = ehdr->e_entry;
+  return EFI_SUCCESS;
 }
--- neoboot/elf_loader.h
+++ neoboot/elf_loader.h
@@ -1,7 +1,13 @@
 #ifndef _BOOT_ELF_LOADER_H
 #define _BOOT_ELF_LOADER_H
 
+#define _NEOBOOT_ELF_LOADER 1
+
 // Prototype
 EFI_STATUS load_kernel(EFI_FILE_PROTOCOL *root, EFI_FILE_PROTOCOL *kernel_file, UINTN kernel_file_size, EFI_PHYSICAL_ADDRESS kernel_base_addr);
+
+#ifdef _NEOBOOT_ELF_LOADER
+EFI_STATUS load_kernel(EFI_FILE_PROTOCOL *root, EFI_FILE_PROTOCOL *kernel_file, UINTN kernel_file_size, EFI_PHYSICAL_ADDRESS kernel_base_addr, Elf64_Addr *entry_addr);
+#endif
 
 #endif //_BOOT_ELF_LOADER_H
