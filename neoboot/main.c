904b93286efaf904734831595e120f82a34670c5
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-12-03 14:00:59 +0900

    こんな感じでOK

--- neoboot/main.c
+++ neoboot/main.c
@@ -66,8 +66,12 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE IM, EFI_SYSTEM_TABLE *sys_table) {
     // Allocate the kernel file
     EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
     allocate_kernel(kernel_f_size, &kernel_base_addr);
+    PrintOK();
+    Print(L"Allocate kernel \n");
     // Read Kernel
     read_kernel(kernel_f, &kernel_f_size, &kernel_base_addr);
+    PrintOK();
+    Print(L"Read kernel \n");
     Print(L"[ INFO ] Kernel File Info \n Size: %lu \n Address: %lx", kernel_f_size, kernel_base_addr);
     // Halt
     while (1) __asm__ ("hlt");
