498fefb1205736074f6f41496863fd500fce1682
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-12-03 13:01:26 +0900

    とりあえずどうだろう

--- neoboot/main.c
+++ neoboot/main.c
@@ -37,13 +37,14 @@ void init_uefi(void) {
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE IM, EFI_SYSTEM_TABLE *sys_table) {
     // Welcome
     init_uefi();
-    /* Memory Map関連の処理 */
-    // Init memory map
-    memmap map;
-    init_memmap(&map);
+
     // Open Root Directory
     EFI_FILE_PROTOCOL *root;
     open_root_dir(IM, &root);
+
+    // Init memory map
+    memmap map;
+    init_memmap(&map);
     // Get memory map
     get_memmap(&map);
     // Print memory map
@@ -55,7 +56,32 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE IM, EFI_SYSTEM_TABLE *sys_table) {
     EFI_FILE_PROTOCOL *memmap_f;
     memmap_f = NULL;
     save_memmap(&map, memmap_f, root);
-    /* @END メモリーマップ関係の処理 */
+    // End Memory Map
+    FreePool(map.buffer);
+
+    // Get info about the kernel file
+    EFI_FILE_PROTOCOL* kernel_file;
+    root_dir->Open(
+      root_dir, &kernel_file, L"\\kernel.elf",
+      EFI_FILE_MODE_READ, 0);
+
+    UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
+    UINT8 file_info_buffer[file_info_size];
+    kernel_file->GetInfo(
+        kernel_file, &gEfiFileInfoGuid,
+        &file_info_size, file_info_buffer);
+
+    EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
+    UINTN kernel_file_size = file_info->FileSize;
+
+    // Allocate kernel
+    EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
+    gBS->AllocatePages(
+      AllocateAddress, EfiLoaderData,
+      (kernel_file_size + 4095) / 4096, &kernel_base_addr);
+    kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
+    Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
+
     
     // Halt
     while (1) __asm__ ("hlt");
