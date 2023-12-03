08dc41b001e0d17c40903b943c378240bce46c14
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-12-03 11:32:24 +0900

    これでいいかな

--- neoboot/elf_loader.c
+++ neoboot/elf_loader.c
@@ -25,13 +25,13 @@ EFI_STATUS read_file(UINTN *file_size, EFI_FILE_PROTOCOL *file, void *buffer) {
     }
     PrintOK();
     Print(L"Allocate pool\n");
-    // // Read File
-    // status = file->Read(file, file_size, &buffer);
-    // if (EFI_ERROR(status)) {
-    //     PrintError();
-    //     Print(L"Read Kernel \n");
-    // }
-    // PrintOK();
-    // Print(L"Read kernel\n");
+    // Read File
+    status = file->Read(file, file_size, &buffer);
+    if (EFI_ERROR(status)) {
+        PrintError();
+        Print(L"Read Kernel \n");
+    }
+    PrintOK();
+    Print(L"Read kernel\n");
     return EFI_SUCCESS;
 }
