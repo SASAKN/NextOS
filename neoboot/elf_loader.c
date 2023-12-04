ea23ac0f0fdc654388920ecad62630c6e9072bba
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-12-04 17:03:50 +0900

    ELFローダーの無駄部分の削除

--- neoboot/elf_loader.c
+++ neoboot/elf_loader.c
@@ -13,32 +13,3 @@
 #include "elf.h"
 #include "elf_loader.h"
 
-void CalcLoadAddressRange(elf64_ehdr* ehdr, UINT64* first, UINT64* last) {
-  elf64_phdr* phdr = (elf64_phdr*)((UINT64)ehdr + ehdr->e_phoff);
-  *first = MAX_UINT64;
-  *last = 0;
-  for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i) {
-    if (phdr[i].p_type != PT_LOAD) continue;
-    *first = MIN(*first, phdr[i].p_vaddr);
-    *last = MAX(*last, phdr[i].p_vaddr + phdr[i].p_memsz);
-  }
-}
-
-void CopyLoadSegments(elf64_ehdr* ehdr) {
-  elf64_phdr* phdr = (elf64_phdr*)((UINT64)ehdr + ehdr->e_phoff);
-  for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i) {
-    if (phdr[i].p_type != PT_LOAD) continue;
-
-    UINT64 segm_in_file = (UINT64)ehdr + phdr[i].p_offset;
-    CopyMem((VOID*)phdr[i].p_vaddr, (VOID*)segm_in_file, phdr[i].p_filesz);
-
-    UINTN remain_bytes = phdr[i].p_memsz - phdr[i].p_filesz;
-    SetMem((VOID*)(phdr[i].p_vaddr + phdr[i].p_filesz), remain_bytes, 0);
-  }
-}
-
-// ELFローダー
-EFI_STATUS load_kernel(EFI_FILE_PROTOCOL *root, EFI_FILE_PROTOCOL *kernel_file, UINTN kernel_file_size, EFI_PHYSICAL_ADDRESS kernel_base_addr, Elf64_Addr *entry_addr) {
-  return EFI_SUCCESS;
-
-}
--- neoboot/elf_loader.h
+++ neoboot/elf_loader.h
@@ -2,8 +2,5 @@
 #define _BOOT_ELF_LOADER_H
 
 // Prototype
-EFI_STATUS load_kernel(EFI_FILE_PROTOCOL *root, EFI_FILE_PROTOCOL *kernel_file, UINTN kernel_file_size, EFI_PHYSICAL_ADDRESS kernel_base_addr, Elf64_Addr *entry_addr);
-void CopyLoadSegments(elf64_ehdr* ehdr);
-void CalcLoadAddressRange(elf64_ehdr* ehdr, UINT64* first, UINT64* last);
 
 #endif //_BOOT_ELF_LOADER_H
