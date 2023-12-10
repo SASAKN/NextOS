9b5915a5a2b8fc4b29d2e69e314ef29a1fed4d08
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-12-10 17:08:43 +0900

    アーキテクチャー依存のあるソースコードは、全て種類別に保存

--- arch/arch.h
+++
@@ -1,6 +0,0 @@
-#include "include.h"
-
-/* Defines */
-#define _ARCH_BOOT   _NEOS_BOOT_SYSTEM
-#define _ARCH_CHIP   _NEOS_CHIP
-#define _ARCH_ARCH   _NEOS_MACHINE
--- arch/include.h
+++
@@ -1,1 +0,0 @@
-#include "../include/neos/machine.h"
