6cb31cd917e647c988de4086f4f6b98607a47bf4
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-12-10 17:41:24 +0900

    こんな感じでいいはず

--- kernel/build_kernel.sh
+++ kernel/build_kernel.sh
@@ -3,8 +3,11 @@
 # Kernel Build Tool
 
 # First, build the kernel file
-clang -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -c ${1}
+clang -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -c ${1} -o kernel.o
 ld.lld --entry kernel_main -z norelro --image-base 0x100000 --static -o kernel.elf kernel.o
 
 # First Output File : kernel.elf 
 # This is the neos kernel file
+
+# Remove Temporary file
+rm kernel.o
