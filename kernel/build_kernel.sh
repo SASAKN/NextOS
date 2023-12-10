b627dba94f116be5af61ae62d52fe2a056ae52d4
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-12-10 17:38:06 +0900

    ビルドファイルのコミットを追加

--- kernel/build_kernel.sh
+++ kernel/build_kernel.sh
@@ -2,7 +2,9 @@
 
 # Kernel Build Tool
 
-# カーネルをビルドします。
-
+# First, build the kernel file
 clang -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -c ${1}
 ld.lld --entry kernel_main -z norelro --image-base 0x100000 --static -o kernel.elf kernel.o
+
+# First Output File : kernel.elf 
+# This is the neos kernel file
