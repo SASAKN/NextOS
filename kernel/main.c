2fb246826c7cd497a0f3bb657bb9cf5727924bd3
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-12-17 14:27:59 +0900

    フレームバッファー

--- kernel/main.c
+++ kernel/main.c
@@ -1,4 +1,4 @@
 //この関数がジャンプされてすぐに実行される
 void kernel_main(void) {
-    while(1) __asm__ volatile("hlt");
+    while(1) __asm__ volatile ("hlt");
 }
--- neoboot/graphics.h
+++ neoboot/graphics.h
@@ -1,4 +1,21 @@
 #pragma once
 #include <Uefi/UefiBaseType.h>
 
+
+enum pixel_format {
+    kPixelRGBResv8BitPerColor,
+    kPixelBGRResv8BitPerColor,
+};
+
+
+// Frame buffer configuration
+struct fb_configuration {
+    UINT32 hr; //水平の画質
+    UINT32 vr; //垂直の画質
+    EFI_PHYSICAL_ADDRESS base_addr; //フレームバッファーへのアドレス
+    UINT32 pixels_per_scan_line; // 1ピクセルに必要なバイト数
+    enum pixel_format pf; //ピクセルフォーマットの指定
+};
+
+
 EFI_STATUS open_gop(EFI_HANDLE IM, EFI_GRAPHICS_OUTPUT_PROTOCOL **gop);
