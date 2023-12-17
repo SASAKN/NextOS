c91a68a6236874071d3405950ea3771d1d49bd65
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-12-17 15:23:49 +0900

    Kernel Mainの関数の処理を変更します

--- kernel/main.c
+++ kernel/main.c
@@ -1,10 +1,12 @@
 #include "graphics.h"
 
 //この関数がジャンプされてすぐに実行される
-void kernel_main(struct fb_configuration fb_config) {
+
+
+
+void kernel_main(unsigned char *fb, unsigned long long fb_size) {
     //白を塗りつぶす
-    unsigned char *fb = (unsigned char *)fb_config.base_addr;
-    for (unsigned long long i = 0; i < fb_config.fb_size; ++i) {
+    for (unsigned long long i = 0; i < fb_size; ++i) {
         fb[i] = 255;
     }
     //処理が終わると
