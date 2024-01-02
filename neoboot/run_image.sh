a6a0bc7752fc0752949baf72e297b9b97424cb5d
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-01-02 12:36:23 +0900

    Legacy BIOSに対応しません

--- neoboot/run_image.sh
+++ neoboot/run_image.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
+# 固定変数（変えると動かなくなる）
 script_dir="$(dirname "$(readlink -f "$0")")"
-
-#    -nographic -serial mon:stdio
 
 # ディスクイメージファイルのパス
 image_file="${script_dir}/neos.img"
@@ -21,8 +20,9 @@ qemu-system-x86_64 \
     -drive if=ide,index=0,media=disk,format=raw,file=${script_dir}/neos.img \
     -device nec-usb-xhci,id=xhci \
     -device usb-mouse -device usb-kbd \
-    -monitor stdio
+    -nographic -serial mon:stdio
 
 # -monitor stdio　//GUIを使いたい場合
+# -nographic -serial mon:stdio //CUIで、シリアルの出力と入力を受け取ったり、送信したりしたい場合
 
 #GUIを使用していないときに、終了したい時はctrl a xで、終了できます
