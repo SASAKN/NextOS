a3f88c80f12954ee2394151f78256932b7792a07
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-01-17 22:48:45 +0900

    こんな感じで作った

--- kernel/main.c
+++ kernel/main.c
@@ -8,6 +8,14 @@ void kernel_main(struct _boot_param bp) {
     for( unsigned long long i = 0; i < bp.fb_setting.fb_size; i++ ) {
         fb[i] = 255;
     }
-    while(1);
+
+    //とりあえず、Printせずにできるもので
+    __asm__ volatile ("movl %0 %%rdi;"
+                      : "r"(bp.fb_setting.base_addr));
+    __asm__ volatile ("movl %0 %%rsi;"
+                      : "r"(bp.fb_setting.fb_size));
+    __asm__ volatile ("movl %0 %%rdx;"
+                      : "r"(bp.fb_setting.pixels_per_scan_line));
+    while(1) __asm__ volatile ("hlt");
 };
 
