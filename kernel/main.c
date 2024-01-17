d09890107bc1844404c15f47bf718554d3cd3528
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-01-17 22:18:16 +0900

    例外が発生する

--- kernel/main.c
+++ kernel/main.c
@@ -3,7 +3,11 @@
 
 //この関数がジャンプされてすぐに実行される
 void kernel_main(struct _boot_param *bp) {
-    
+    //白く塗りつぶしてみるか
+    unsigned char *fb = (unsigned char *)bp->fb_setting.base_addr;
+    for( unsigned long long i = 0; i < bp->fb_setting.fb_size; i++ ) {
+        fb[i] = 255;
+    }
     while(1);
 };
 
