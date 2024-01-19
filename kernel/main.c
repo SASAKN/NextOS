b2ccbf98a7550be8c841a98e6dc60a61c46b3354
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-01-19 18:24:34 +0900

    このように変更

--- kernel/main.c
+++ kernel/main.c
@@ -1,10 +1,8 @@
 #include <neos/types.h>
 
 
-
 void kernel_main(void) {
-    char a = 'a';
-    arch_serial_write(a);
+    
     while(1) __asm__ ("hlt");
 }
 
--- neoboot/graphics.h
+++ neoboot/graphics.h
@@ -17,7 +17,7 @@ typedef struct _fb_config {
     unsigned int hr; //水平の画質
     unsigned int vr; //垂直の画質
     unsigned long long fb_size; //フレームバッファーのサイズ
-    unsigned long long base_addr; //フレームバッファーへのアドレス
+    unsigned char *base; //フレームバッファー
     unsigned int pixels_per_scan_line; // 1ピクセルに必要なバイト数
     enum pixel_format pf; //ピクセルフォーマットの指定
 } fb_config;
