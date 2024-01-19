1ec8374617dab1719fd22aa085bfd365708685da
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-01-19 17:58:16 +0900

    ビルドシステムの刷新

--- neoboot/move_to_linux.sh
+++ neoboot/move_to_linux.sh
@@ -4,6 +4,6 @@
 
 script_dir="$(dirname "$(readlink -f "$0")")"
 
-rsync -av --progress --exclude 'README.md' --exclude '.git/' ${script_dir}/ /Users/kenta/edk2/neoboot
+rsync -av --progress --exclude 'README.md' --exclude '.git/' --exclude '*.img' --exclude '*.dmg' ${script_dir}/ /Users/kenta/edk2/neoboot
 
 
