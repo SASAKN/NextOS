d26c79d01846e0c5ff44ffbeb947ce8020913b54
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-01-19 17:42:23 +0900

    これでよし

--- kernel/build_kernel.sh
+++ kernel/build_kernel.sh
@@ -2,12 +2,17 @@
 
 # Kernel Build Tool
 
+script_dir="$(dirname "$(readlink -f "$0")")"
+
 # First, build the kernel file
 clang -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -c ${1} -o kernel.o
 ld.lld --entry kernel_main -z norelro --image-base 0x100000 --static -o kernel.elf kernel.o
 
 # First Output File : kernel.elf 
 # This is the neos kernel file
+
+if [$? = 0]; then
+    cp ${script_dir}/kernel.elf ${script_dir}/../neoboot/kernel.elf
 
 # Remove Temporary file
 rm kernel.o
--- kernel/main.c
+++ kernel/main.c
@@ -4,7 +4,7 @@
 //この関数がジャンプされてすぐに実行される
 void kernel_main(const struct _boot_param *bp) {
     unsigned char *fb = (unsigned char *)bp->fb_setting.base_addr;
-    for (UINTN i = 0; i < bp->fb_setting.fb_size; i++; ) {
+    for (unsigned long long i = 0; i < bp->fb_setting.fb_size; i++) {
         fb[i] = 255;
     }
     while(1) __asm__ volatile ("hlt");
