8e5c59fafb260247ffc36c6c258a0be13187062f
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-01-21 14:44:48 +0900

    hankakuも削除。

--- kernel/include/proto.h
+++ kernel/include/proto.h
@@ -1,10 +1,6 @@
 #include <neos/types.h>
 #include "graphics.h"
 
-// Text Functions
-const uint8_t *get_font(char c);
-void write_ascii(const fb_config *fb_con, int x, int y, char c, const fb *fb);
-
 // Graphics Functions
 void write_pixel(const fb_config *fb_con, uint32_t x, uint32_t y, const fb *fb);
 
--- kernel/text.c
+++ kernel/text.c
@@ -2,30 +2,3 @@
 #include "include/proto.h"
 #include "include/graphics.h"
 #include "include/font.h"
-
-//font.oにある定数
-extern const uint8_t _binary_hankaku_bin_start;
-extern const uint8_t _binary_hankaku_bin_end;
-extern const uint8_t _binary_hankaku_bin_size;
-
-const uint8_t *get_font(char c) {
-    uintptr_t index = 16 * (unsigned int)c;
-    if (index >= (uintptr_t)&_binary_hankaku_bin_size) {
-        return NULL;
-    }
-    return &_binary_hankaku_bin_start + index;
-}
-
-void write_ascii(const fb_config *fb_con, int x, int y, char c, const fb *fb) {
-    const uint8_t *font = get_font(c);
-    if (font == NULL) {
-        return;
-    }
-    for (int dy = 0; dy < FONT_SIZE_Y; dy++) {
-        for (int dx = 0; dx < FONT_SIZE_X; dx++) {
-            if ((font[dy] << dx) & 0x80u) {
-                write_pixel(fb_con, x + dx, y + dy, fb);
-            }
-        }
-    }
-}
