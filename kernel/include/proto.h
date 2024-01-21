6d73024ca3987dbd1666b8af6d680264db2f351b
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-01-21 10:09:07 +0900

    こんな感じでかいた

--- kernel/include/proto.h
+++ kernel/include/proto.h
@@ -1,4 +1,9 @@
 // Prototype Functions
+#include <neos/types.h>
+#include "graphics.h"
 
 // HAL Functions - これに合わせてHALを設計するべき。
 void halt(void);
+
+// Graphics Functions
+void write_pixel(const fb_config *fb_con, uint32_t x, uint32_t y, const fb *fb);
