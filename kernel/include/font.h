d394a1c89fedaec37926fa55f62dc54388d69b23
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-01-21 13:24:42 +0900

    文字表示関数を書いた

---
+++ kernel/include/font.h
@@ -0,0 +1,5 @@
+#pragma once
+
+//文字のデフォルトサイズ
+#define FONT_SIZE_X 8
+#define FONT_SIZE_Y 16
--- kernel/text.c
+++ kernel/text.c
@@ -1,6 +1,7 @@
 #include <neos/types.h>
 #include "include/proto.h"
 #include "include/graphics.h"
+#include "include/font.h"
 
 //font.oにある定数
 extern const _binary_font_bin_start;
@@ -13,4 +14,18 @@ const uint8_t *get_font(char c) {
         return NULL;
     }
     return &_binary_font_bin_start + index;
+}
+
+void write_ascii(const fb_config *fb_con, int x, int y, char c, const fb *fb) {
+    const uint8_t font = get_font(c);
+    if (font == NULL) {
+        return;
+    }
+    for (int dy = 0; dy < FONT_SIZE_Y; dy++) {
+        for (int dx = 0; dx < FONT_SIZE_X; dx++) {
+            if ((font[dy] << dx) & 0x80u) {
+                write_pixel(fb_con, x + dx, y + dy, fb);
+            }
+        }
+    }
 }
