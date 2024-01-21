35e4b7f9870bdd77bda03631a05a2c83f1f440a7
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-01-21 10:01:44 +0900

    これでビルドが動く

--- kernel/build_kernel.sh
+++ kernel/build_kernel.sh
@@ -5,7 +5,7 @@
 script_dir="$(dirname "$(readlink -f "$0")")"
 
 # First, build the kernel file
-clang -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -I ../include/ -c ${1} -o kernel.o
+clang -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fshort-wchar -fno-rtti -I ../include/ -c $(cat ${script_dir}/complile_file.list)
 ld.lld --entry kernel_main -z norelro --image-base 0x100000 --static -o kernel.elf kernel.o
 
 # First Output File : kernel.elf 
@@ -14,5 +14,6 @@ ld.lld --entry kernel_main -z norelro --image-base 0x100000 --static -o kernel.e
 if [ $? = 0 ]; then
     rm kernel.o
     cp ${script_dir}/kernel.elf ${script_dir}/../neoboot/kernel.elf
-    cd ${script_dir}/../neoboot/
+else
+    echo "[ EROOR ] Can not create the kernel file"
 fi
--- kernel/graphics.c
+++ kernel/graphics.c
@@ -4,6 +4,8 @@
 
 //マイクロカーネルのため、これは、ユーザーモードで実行されるようにいつかなります。
 // しばらくの間は、カーネルモードで、FBを実行するようにします。
+// BootFSに対応したら、FBをユーザーモードで実行します
+
 
 // Write Pixel
 void write_pixel(const fb_config *fb_con, uint32_t x, uint32_t y, const fb *fb) {
@@ -15,7 +17,7 @@ void write_pixel(const fb_config *fb_con, uint32_t x, uint32_t y, const fb *fb) 
         pixel[1] = fb->g; // GREEN
         pixel[2] = fb->b; // BLUE
     } else if (fb_con->pf == efi_bgr) {
-        //PixelBlueGreenRedReserved8BitPerColor
+        // PixelBlueGreenRedReserved8BitPerColor
         unsigned char *pixel = fb_con->base[4 * pixel_position];
         pixel[0] = fb->b; // BLUE
         pixel[1] = fb->g; // GREEN
